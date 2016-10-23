2fc4791f4f90bf4b4d4275fffd09d590230f81a9
Diego Muracciole <diegomuracciole@gmail.com>
2016-10-23 03:03:44 -0300

    Pass props to addPage
    Fixes #12
    Fixes #15

--- examples/example.jsx
+++ examples/example.jsx
@@ -5,7 +5,7 @@ import lorem from './lorem';
 let doc =
   <document title="Lorem Ipsum" author="@diegomura" otherData="Something else">
     <page margin={50}>
-      <image src="examples/images/react.png" x={200} y={300} width={200} />
+      <image src="examples/images/react.png" x={200} y={300} width={200}/>
       <text align="center" underline>
         ~ Lorem ipsum ~
       </text>
--- src/wrappers/PageComponent.js
+++ src/wrappers/PageComponent.js
@@ -7,10 +7,13 @@ const PageComponent = function(element, context) {
 
 const PageComponentMixin = {
   mountComponent() {
+    const node = this._element;
+    const {children, ...props} = node.props;
+
     // Because the document already starts with a page
     // we don't call addPage the first time
     if (this._context.firstPageSkipped)
-      this._context.doc.addPage();
+      this._context.doc.addPage(props);
 
     this._context.firstPageSkipped = true;
   }

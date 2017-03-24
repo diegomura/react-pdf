166c25f2c87e8e52a2ffb50144db4d2404adabcc
Jaco Bovenschen <jbovenschen@leukeleu.nl>
2017-03-24 13:47:01 +0100

    Rename standalone.jsx to standalone.js

--- examples/standalone/example.js
+++ examples/standalone/example.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactPDF from 'react-pdf';
 import lorem from './lorem';
 
-let doc = (
-  <document title="Lorem Ipsum" author="@diegomura" otherData="Something else">
+const doc = (
+  <document author="@diegomura" otherData="Something else" title="Lorem Ipsum">
     <page margin={50}>
-      <image src="./images/react.png" x={200} y={300} width={200} />
+      <image src="./images/react.png" width={200} x={200} y={300} />
       <text align="center" underline>
         ~ Lorem ipsum ~
       </text>
-      <text columns={3} columnGap={15} align="justify">
+      <text align="justify" columnGap={15} columns={3}>
         {lorem}
       </text>
     </page>

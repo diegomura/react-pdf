0a4f47f9b438784c735652cf4a501d6a123c46ed
Jaco Bovenschen <jbovenschen@leukeleu.nl>
2017-03-24 10:02:55 +0100

    Run prettier over source files

--- examples/example.jsx
+++ examples/example.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactPDF from '../';
 import lorem from './lorem';
 
-let doc =
+let doc = (
   <document title="Lorem Ipsum" author="@diegomura" otherData="Something else">
     <page margin={50}>
       <image src="examples/images/react.png" x={200} y={300} width={200} />
       <text align="center" underline>
         ~ Lorem ipsum ~
       </text>
-      <text columns={3} columnGap={15} align='justify'>
+      <text columns={3} columnGap={15} align="justify">
         {lorem}
       </text>
     </page>
   </document>
+);
 
 ReactPDF.render(doc, './examples/example.pdf');

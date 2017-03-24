29a4a55dcdf674e644a39959519cf0374f5501e0
Jaco Bovenschen <jbovenschen@leukeleu.nl>
2017-03-24 09:43:07 +0100

    Move examples to separate folder so we can support multiple examples

--- examples/standalone/example.jsx
+++ examples/standalone/example.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactPDF from '../';
+import ReactPDF from 'react-pdf';
 import lorem from './lorem';
 
-let doc =
+let doc = (
   <document title="Lorem Ipsum" author="@diegomura" otherData="Something else">
     <page margin={50}>
-      <image src="examples/images/react.png" x={200} y={300} width={200} />
+      <image src="./images/react.png" x={200} y={300} width={200} />
       <text align="center" underline>
         ~ Lorem ipsum ~
       </text>
-      <text columns={3} columnGap={15} align='justify'>
+      <text columns={3} columnGap={15} align="justify">
         {lorem}
       </text>
     </page>
   </document>
+);
 
-ReactPDF.render(doc, './examples/example.pdf');
+ReactPDF.render(doc, './example.pdf');

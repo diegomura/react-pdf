b8648f8fde37b7a3ffebd5eccdf4c2ab7b432a00
Jaco Bovenschen <jbovenschen@leukeleu.nl>
2017-05-20 14:55:39 +0200

    Rename some internal instances to match component names:
    Document => Root
    Pages => Document
    
    This makes it more clear where what happens

--- packages/react-pdf/src/elements/Document.js
+++ packages/react-pdf/src/elements/Document.js
@@ -1,77 +1,18 @@
-import Catalog from './Catalog';
-import Font from './Font';
-import { pdfDictionary } from '../utils/pdf';
+import PDFEntry from './PDFEntry';
+import { pdfObject } from '../utils/pdf';
 
-class Document {
-  constructor(props) {
-    this.nodes = [];
-    this.header = '%PDF-1.7\n%����\n';
-    this.offset = this.header.length;
-
-    this.font = new Font(null, this);
-    this.catalog = new Catalog(props, this);
-    this.catalog.parent = this;
-  }
-
-  appendChild(child) {
-    this.catalog.appendChild(child);
-  }
-
-  removeChild(child) {
-    this.catalog.removeChild(child);
-  }
-
-  addNode(node) {
-    this.nodes.push(node);
-  }
-
-  addOffset(offset) {
-    const currentOffset = this.offset;
-    this.offset += offset;
-
-    return currentOffset;
-  }
-
-  renderReferenceEntry(node) {
-    return `${('0000000000' + node.offset).slice(-10)} 00000 n`;
-  }
-
-  renderReferenceTable() {
-    const references = this.nodes
-      .sort((a, b) => a.id > b.id)
-      .map(this.renderReferenceEntry)
-      .join('\n') + '\n';
-
-    return [
-      'xref',
-      `0 ${this.nodes.length + 1}`,
-      '0000000000 65535 f',
-      references,
-    ].join('\n');
-  }
-
-  renderTrailer() {
-    return [
-      'trailer',
-      pdfDictionary({
-        Size: this.nodes.length,
-        Root: this.catalog.ref(),
-      }),
-      'startxref',
-      this.offset,
-      '%%EOF',
-    ].join('\n');
-  }
-
+class Pages extends PDFEntry {
   render() {
-    return [
-      this.header,
-      this.font.render(),
-      this.catalog.render(),
-      this.renderReferenceTable(),
-      this.renderTrailer(),
-    ].join('');
+    const childObjects = this.children.map(child => child.ref());
+
+    return super.render(
+      pdfObject(this.id, {
+        Type: '/Pages',
+        Kids: `[${childObjects.join(' ')}]`,
+        Count: this.children.length,
+      }) + '\n',
+    );
   }
 }
 
-export default Document;
+export default Pages;

106c92b17804ead99268eee0fd550d6d49112aae
Diego Muracciole <diegomuracciole@gmail.com>
2017-06-27 09:10:49 -0300

    Mock examples date (#103)

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,9 @@
+import MockDate from 'mockdate';
+
+// For git purposes.
+// Prevent commiting PDF example everytime the script is excecuted.
+MockDate.set(1434319925275);
+
 try {
   require(`./${process.argv[2]}/index`);
 } catch (e) {

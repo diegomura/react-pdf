57e672f21f7b9561bb23bde861bd788b9162cde0
Mark Langovoi <marklangovoi@gmail.com>
2018-01-18 00:26:16 +0300

    Make dom-bindings work again!

--- examples/dom-bindings/src/index.js
+++ examples/dom-bindings/src/index.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill'; // to fix `regeneratorRuntime is not defined`, which causes by `@react-pdf/*`
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';

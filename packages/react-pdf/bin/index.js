ef640071760a51bebc52c2cf7693493a00c9a346
Mark Langovoi <langovoi@users.noreply.github.com>
2018-01-17 21:41:03 +0300

    Remove precompiled yoga (#169)
    * Update .gitignore
    
    * Remove precompiled yoga-layout
    
    * Enable linux for testing

--- packages/react-pdf/bin/index.js
+++
@@ -1,6 +0,0 @@
-const nbind = require('nbind');
-
-const nodeVersion = process.version.match(/^v(\d+)/)[1];
-const ret = nbind.init(__dirname + '/' + nodeVersion);
-
-module.exports = require('./entry-common')(ret.bind, ret.lib);

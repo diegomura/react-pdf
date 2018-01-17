812753e0960d63037b38f4c20ead3b0961bdb2fd
Mark Langovoi <langovoi@users.noreply.github.com>
2018-01-18 01:34:31 +0300

    Fix core and dom packages (#170)

---
+++ examples/dom-bindings/scripts/test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+process.env.NODE_ENV = 'test';
+process.env.PUBLIC_URL = '';
+
+// Load environment variables from .env file. Suppress warnings using silent
+// if this file is missing. dotenv will never modify any environment variables
+// that have already been set.
+// https://github.com/motdotla/dotenv
+require('dotenv').config({ silent: true });
+
+const jest = require('jest');
+const argv = process.argv.slice(2);
+
+// Watch unless on CI or in coverage mode
+if (!process.env.CI && argv.indexOf('--coverage') < 0) {
+  argv.push('--watch');
+}
+
+jest.run(argv);

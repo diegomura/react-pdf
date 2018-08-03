c228d7bbcd60c9509f22bb11d91d62df46f8eada
Diego Muracciole <diegomuracciole@gmail.com>
2018-08-02 23:06:48 -0300

    v1.0.0-alpha.12

--- rollup.config.js
+++ rollup.config.js
@@ -28,35 +28,14 @@ const babelConfig = ({ browser }) => ({
   presets: [
     [
       'env',
-      {
-        loose: true,
-        modules: false,
-        targets: browser
-          ? {
-              browsers: [
-                '> 0.05%',
-                'not dead',
-                'not ie 11',
-                'not android 4.1',
-                'not android 4.2',
-                'not android 4.4',
-                'not android 4.4.3',
-                'not chrome 29',
-                'not chrome 43',
-                'not chrome 49',
-                'not chrome 54',
-                'not firefox 47',
-                'not firefox 48',
-                'not firefox 51',
-                'not firefox 52',
-                'not ios 8.1',
-                'not ios 9.3',
-                'not safari 5.1',
-                'not safari 9.1',
-              ],
-            }
-          : { node: '8.11.3' },
-      },
+      Object.assign(
+        {},
+        {
+          loose: true,
+          modules: false,
+        },
+        browser ? {} : { targets: { node: '8.11.3' } },
+      ),
     ],
     'react',
   ],
@@ -85,6 +64,12 @@ const configBase = {
     'babel-runtime/core-js/object/assign',
     'babel-runtime/helpers/asyncToGenerator',
     'babel-runtime/core-js/get-iterator',
+    'babel-runtime/helpers/inherits',
+    'babel-runtime/helpers/classCallCheck',
+    'babel-runtime/helpers/possibleConstructorReturn',
+    'babel-runtime/regenerator',
+    'babel-runtime/helpers/createClass',
+    'babel-runtime/helpers/typeof',
   ].concat(Object.keys(pkg.dependencies), Object.keys(pkg.peerDependencies)),
   plugins: commonPlugins,
   sourcemap: true,

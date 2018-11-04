1641ebd4b5e81f91332bd1486a5182cf10f75821
Diego Muracciole <diegomuracciole@gmail.com>
2018-11-04 14:59:33 -0300

    WIP: Textkit refactoring

--- src/layout/index.js
+++ src/layout/index.js
@@ -12,6 +12,7 @@ const shrinkWhitespaceFactor = { before: -0.5, after: -0.5 };
 export class LayoutEngine extends BaseLayoutEngine {
   constructor({ hyphenationCallback, hyphenationPenalty }) {
     const engines = {
+      wordHyphenation: wordHyphenation,
       scriptItemizer: scriptItemizer(),
       decorationEngine: textDecorationEngine(),
       fontSubstitutionEngine: fontSubstitutionEngine(),

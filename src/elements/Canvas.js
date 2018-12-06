1409b094b4201d81615e7df6cf0de33f35398809
Markus Johnsson <markus.johnsson@infviz.com>
2018-12-06 09:33:56 +0100

    Pass width and height to canvas painter

--- src/elements/Canvas.js
+++ src/elements/Canvas.js
@@ -16,9 +16,10 @@ class Canvas extends Base {
     this.applyTransformations();
     this.drawBackgroundColor();
     this.drawBorders();
-    const { left, top } = this.getAbsoluteLayout();
+    const { left, top, width, height } = this.getAbsoluteLayout();
     this.root.instance.translate(left, top);
-    if (this.props.children) this.props.children(painter(this.root.instance));
+    if (this.props.children)
+      this.props.children(painter(this.root.instance), width, height);
     if (this.props.debug) this.debug();
     this.root.instance.restore();
   }

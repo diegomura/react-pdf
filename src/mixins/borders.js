8668c28d891988b4463e77f76434757d438437b8
Diego Muracciole <diegomuracciole@gmail.com>
2019-01-13 21:21:12 -0300

    Enable border styles

--- src/mixins/borders.js
+++ src/mixins/borders.js
@@ -56,25 +56,33 @@ function drawBorders() {
 
   instance.save();
 
-  instance.save();
-  clipBorderTop(instance, layout, style, rtr, rtl);
-  fillBorderTop(instance, layout, style, rtr, rtl);
-  instance.restore();
+  if (borderTopWidth) {
+    instance.save();
+    clipBorderTop(instance, layout, style, rtr, rtl);
+    fillBorderTop(instance, layout, style, rtr, rtl);
+    instance.restore();
+  }
 
-  instance.save();
-  clipBorderRight(instance, layout, style, rtr, rbr);
-  fillBorderRight(instance, layout, style, rtr, rbr);
-  instance.restore();
+  if (borderRightWidth) {
+    instance.save();
+    clipBorderRight(instance, layout, style, rtr, rbr);
+    fillBorderRight(instance, layout, style, rtr, rbr);
+    instance.restore();
+  }
 
-  instance.save();
-  clipBorderBottom(instance, layout, style, rbl, rbr);
-  fillBorderBottom(instance, layout, style, rbl, rbr);
-  instance.restore();
+  if (borderBottomWidth) {
+    instance.save();
+    clipBorderBottom(instance, layout, style, rbl, rbr);
+    fillBorderBottom(instance, layout, style, rbl, rbr);
+    instance.restore();
+  }
 
-  instance.save();
-  clipBorderLeft(instance, layout, style, rbl, rtl);
-  fillBorderLeft(instance, layout, style, rbl, rtl);
-  instance.restore();
+  if (borderLeftWidth) {
+    instance.save();
+    clipBorderLeft(instance, layout, style, rbl, rtl);
+    fillBorderLeft(instance, layout, style, rbl, rtl);
+    instance.restore();
+  }
 
   instance.restore();
 }
@@ -82,8 +90,6 @@ function drawBorders() {
 const clipBorderTop = (ctx, layout, style, rtr, rtl) => {
   const { top, left, width, height } = layout;
   const { borderTopWidth, borderRightWidth, borderLeftWidth } = style;
-
-  if (!borderTopWidth) return;
 
   // Clip outer top border edge
   ctx.moveTo(left + rtl, top);
@@ -183,47 +189,31 @@ const fillBorderTop = (ctx, layout, style, rtr, rtl) => {
   const { top, left, width } = layout;
   const {
     borderTopColor,
-    borderTopStyle,
     borderTopWidth,
+    borderTopStyle,
     borderRightWidth,
     borderLeftWidth,
   } = style;
 
-  const topLeftRadiusX = Math.max(rtl - borderLeftWidth / 2, 0);
-  const topLeftRadiusY = Math.max(rtl - borderTopWidth / 2, 0);
-  const topRightRadiusX = Math.max(rtr - borderRightWidth / 2, 0);
-  const topRightRadiusY = Math.max(rtr - borderTopWidth / 2, 0);
-  const c0 = topLeftRadiusX * (1.0 - KAPPA);
-  const c1 = topLeftRadiusY * (1.0 - KAPPA);
-  const c2 = topRightRadiusX * (1.0 - KAPPA);
-  const c3 = topRightRadiusY * (1.0 - KAPPA);
+  const c0 = rtl * (1.0 - KAPPA);
+  const c1 = rtr * (1.0 - KAPPA);
 
-  ctx.moveTo(left + borderLeftWidth / 2, top + Math.max(rtl, borderTopWidth));
-  ctx.lineTo(left + borderLeftWidth / 2, top + rtl);
+  ctx.moveTo(left, top + Math.max(rtl, borderTopWidth));
+  ctx.bezierCurveTo(left, top + c0, left + c0, top, left + rtl, top);
+  ctx.lineTo(left + width - rtr, top);
   ctx.bezierCurveTo(
-    left + borderLeftWidth / 2,
-    top + borderTopWidth / 2 + c1,
-    left + borderLeftWidth / 2 + c0,
-    top + borderTopWidth / 2,
-    left + rtl,
-    top + borderTopWidth / 2,
-  );
-  ctx.lineTo(left + width - rtr, top + borderTopWidth / 2);
-  ctx.bezierCurveTo(
-    left + width - borderRightWidth / 2 - c2,
-    top + borderTopWidth / 2,
-    left + width - borderLeftWidth / 2,
-    top + borderTopWidth / 2 + c3,
-    left + width - borderRightWidth / 2,
+    left + width - c1,
+    top,
+    left + width,
+    top + c1,
+    left + width,
     top + rtr,
-  );
-  ctx.lineTo(
-    left + width - borderRightWidth / 2,
-    top + Math.max(rtr, borderTopWidth),
   );
 
   ctx.strokeColor(borderTopColor);
-  ctx.lineWidth(Math.max(borderLeftWidth, borderTopWidth, borderLeftWidth));
+  ctx.lineWidth(
+    Math.max(borderRightWidth, borderTopWidth, borderLeftWidth) * 2,
+  );
 
   if (borderTopStyle === 'dashed') {
     ctx.dash(borderTopWidth * 2, { space: borderTopWidth * 1.2 });
@@ -238,8 +228,6 @@ const fillBorderTop = (ctx, layout, style, rtr, rtl) => {
 const clipBorderRight = (ctx, layout, style, rtr, rbr) => {
   const { top, left, width, height } = layout;
   const { borderTopWidth, borderRightWidth, borderBottomWidth } = style;
-
-  if (!borderRightWidth) return;
 
   // Clip outer right border edge
   ctx.moveTo(left + width, top + rtr);
@@ -346,22 +334,56 @@ const clipBorderRight = (ctx, layout, style, rtr, rbr) => {
   }
 };
 
-const fillBorderRight = (ctx, layout, style) => {
+const fillBorderRight = (ctx, layout, style, rtr, rbr) => {
   const { top, left, width, height } = layout;
-  const { borderRightColor } = style;
+  const {
+    borderRightColor,
+    borderRightStyle,
+    borderRightWidth,
+    borderTopWidth,
+    borderBottomWidth,
+  } = style;
 
-  // TODO: Support dotted and dashed styles
-  // Render solid border
-  ctx.rect(left, top, width, height);
-  ctx.fillColor(borderRightColor);
-  ctx.fill();
+  const c0 = rbr * (1.0 - KAPPA);
+  const c1 = rtr * (1.0 - KAPPA);
+
+  ctx.moveTo(left + width - rtr, top);
+  ctx.bezierCurveTo(
+    left + width - c1,
+    top,
+    left + width,
+    top + c1,
+    left + width,
+    top + rtr,
+  );
+  ctx.lineTo(left + width, top + height - rbr);
+  ctx.bezierCurveTo(
+    left + width,
+    top + height - c0,
+    left + width - c0,
+    top + height,
+    left + width - rbr,
+    top + height,
+  );
+
+  ctx.strokeColor(borderRightColor);
+  ctx.lineWidth(
+    Math.max(borderRightWidth, borderTopWidth, borderBottomWidth) * 2,
+  );
+
+  if (borderRightStyle === 'dashed') {
+    ctx.dash(borderRightWidth * 2, { space: borderRightWidth * 1.2 });
+  } else if (borderRightStyle === 'dotted') {
+    ctx.dash(borderRightWidth, { space: borderRightWidth * 1.2 });
+  }
+
+  ctx.stroke();
+  ctx.undash();
 };
 
 const clipBorderBottom = (ctx, layout, style, rbl, rbr) => {
   const { top, left, width, height } = layout;
   const { borderBottomWidth, borderRightWidth, borderLeftWidth } = style;
-
-  if (!borderBottomWidth) return;
 
   // Clip outer top border edge
   ctx.moveTo(left + width - rbr, top + height);
@@ -467,22 +489,56 @@ const clipBorderBottom = (ctx, layout, style, rbl, rbr) => {
   }
 };
 
-const fillBorderBottom = (ctx, layout, style) => {
+const fillBorderBottom = (ctx, layout, style, rbl, rbr) => {
   const { top, left, width, height } = layout;
-  const { borderBottomColor } = style;
+  const {
+    borderBottomColor,
+    borderBottomStyle,
+    borderBottomWidth,
+    borderRightWidth,
+    borderLeftWidth,
+  } = style;
 
-  // TODO: Support dotted and dashed styles
-  // Render solid border
-  ctx.rect(left, top, width, height);
-  ctx.fillColor(borderBottomColor);
-  ctx.fill();
+  const c0 = rbl * (1.0 - KAPPA);
+  const c1 = rbr * (1.0 - KAPPA);
+
+  ctx.moveTo(left + width, top + height - rbr);
+  ctx.bezierCurveTo(
+    left + width,
+    top + height - c1,
+    left + width - c1,
+    top + height,
+    left + width - rbr,
+    top + height,
+  );
+  ctx.lineTo(left + rbl, top + height);
+  ctx.bezierCurveTo(
+    left + c0,
+    top + height,
+    left,
+    top + height - c0,
+    left,
+    top + height - rbl,
+  );
+
+  ctx.strokeColor(borderBottomColor);
+  ctx.lineWidth(
+    Math.max(borderBottomWidth, borderRightWidth, borderLeftWidth) * 2,
+  );
+
+  if (borderBottomStyle === 'dashed') {
+    ctx.dash(borderBottomWidth * 2, { space: borderBottomWidth * 1.2 });
+  } else if (borderBottomStyle === 'dotted') {
+    ctx.dash(borderBottomWidth, { space: borderBottomWidth * 1.2 });
+  }
+
+  ctx.stroke();
+  ctx.undash();
 };
 
 const clipBorderLeft = (ctx, layout, style, rbl, rtl) => {
   const { top, left, width, height } = layout;
   const { borderTopWidth, borderLeftWidth, borderBottomWidth } = style;
-
-  if (!borderLeftWidth) return;
 
   // Clip outer left border edge
   ctx.moveTo(left, top + height - rbl);
@@ -583,14 +639,43 @@ const clipBorderLeft = (ctx, layout, style, rbl, rtl) => {
 };
 
 const fillBorderLeft = (ctx, layout, style, rbl, rtl) => {
-  const { top, left, width, height } = layout;
-  const { borderLeftColor } = style;
+  const { top, left, height } = layout;
+  const {
+    borderLeftColor,
+    borderLeftStyle,
+    borderLeftWidth,
+    borderTopWidth,
+    borderBottomWidth,
+  } = style;
 
-  // TODO: Support dotted and dashed styles
-  // Render solid border
-  ctx.rect(left, top, width, height);
-  ctx.fillColor(borderLeftColor);
-  ctx.fill();
+  const c0 = rbl * (1.0 - KAPPA);
+  const c1 = rtl * (1.0 - KAPPA);
+
+  ctx.moveTo(left + rbl, top + height);
+  ctx.bezierCurveTo(
+    left + c0,
+    top + height,
+    left,
+    top + height - c0,
+    left,
+    top + height - rbl,
+  );
+  ctx.lineTo(left, top + rtl);
+  ctx.bezierCurveTo(left, top + c1, left + c1, top, left + rtl, top);
+
+  ctx.strokeColor(borderLeftColor);
+  ctx.lineWidth(
+    Math.max(borderLeftWidth, borderTopWidth, borderBottomWidth) * 2,
+  );
+
+  if (borderLeftStyle === 'dashed') {
+    ctx.dash(borderLeftWidth * 2, { space: borderLeftWidth * 1.2 });
+  } else if (borderLeftStyle === 'dotted') {
+    ctx.dash(borderLeftWidth, { space: borderLeftWidth * 1.2 });
+  }
+
+  ctx.stroke();
+  ctx.undash();
 };
 
 export default { drawBorders };

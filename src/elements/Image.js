985d25fe22155aad0ad413075275e8760a11b358
Diego Muracciole <diegomuracciole@gmail.com>
2019-01-16 01:06:16 -0300

    Remove log

--- src/elements/Image.js
+++ src/elements/Image.js
@@ -134,8 +134,6 @@ class Image extends Base {
         this.image.height,
       );
 
-      console.log(width, height, xOffset);
-
       if (width !== 0 && height !== 0) {
         this.root.instance.image(
           this.image.data,

80bb5cddb6a25aadba8cfc3a0d225b3a5a444fc8
Andrew Hansen <ahansen@axon.com>
2019-01-23 15:40:07 -0800

    feat(Text): add support for maxLines

--- tests/text.test.js
+++ tests/text.test.js
@@ -28,4 +28,33 @@ describe('Text', () => {
     expect(text.layoutEngine.layout.mock.calls).toHaveLength(1);
     expect(text.layoutEngine.layout.mock.calls[0][0].string).toBe('');
   });
+
+  test('Should render maxLines', async () => {
+    const doc = new Document(dummyRoot, {});
+    const page = new Page(dummyRoot, {});
+    const text = new Text(dummyRoot, { maxLines: 2 });
+    const textInstance = new TextInstance(dummyRoot, 'really long text');
+
+    text.layoutEngine = {
+      layout: jest.fn((attributedString, containers) => {
+        containers[0].blocks = [
+          {
+            lines: [
+              { rect: { y: 0, x: 0 } },
+              { rect: { y: 0, x: 0 } },
+              { rect: { y: 0, x: 0 } },
+            ],
+          },
+        ];
+      }),
+    };
+
+    doc.appendChild(page);
+    page.appendChild(text);
+    text.appendChild(textInstance);
+
+    await text.layoutText();
+
+    expect(text.lines.length).toEqual(2);
+  });
 });

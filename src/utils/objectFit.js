7f89e5f3bdb20b8a25d0a66dd05a24772e31f6be
Diego Muracciole <diegomuracciole@gmail.com>
2019-01-24 19:11:33 -0300

    Support fixed object position

--- src/utils/objectFit.js
+++ src/utils/objectFit.js
@@ -1,3 +1,7 @@
+const isNumeric = n => {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
 const applyContainObjectFit = (cw, ch, iw, ih, px, py) => {
   const cr = cw / ch;
   const ir = iw / ih;
@@ -5,57 +9,52 @@ const applyContainObjectFit = (cw, ch, iw, ih, px, py) => {
   if (cr > ir) {
     const height = ch;
     const width = height * ir;
-    const xOffset = (cw - width) * 0.5;
-    return { width, height, xOffset, yOffset: 0 };
+    const xOffset = isNumeric(px) ? px : (cw - width) * 0.5;
+    return { width, height, xOffset, yOffset: py || 0 };
   } else {
     const width = cw;
     const height = width / ir;
-    const yOffset = (ch - height) * 0.5;
-    return { width, height, yOffset, xOffset: 0 };
+    const yOffset = isNumeric(py) ? py : (ch - height) * 0.5;
+    return { width, height, yOffset, xOffset: px || 0 };
   }
 };
 
-const applyNoneObjectFit = (cw, ch, iw, ih) => {
+const applyNoneObjectFit = (cw, ch, iw, ih, px, py) => {
   const width = iw;
   const height = ih;
-  const xOffset = (cw - width) * 0.5;
-  const yOffset = (ch - height) * 0.5;
+  const xOffset = isNumeric(px) ? px : (cw - width) * 0.5;
+  const yOffset = isNumeric(py) ? py : (ch - height) * 0.5;
 
   return { width, height, xOffset, yOffset };
 };
 
-export const resolveObjectFit = (
-  type = 'fill',
-  cw,
-  ch,
-  iw,
-  ih,
-  px = '50%',
-  py = '50%',
-) => {
+export const resolveObjectFit = (type = 'fill', cw, ch, iw, ih, px, py) => {
   const ir = iw / ih;
   const cr = cw / ch;
 
+  const xOffset = px || 0;
+  const yOffset = py || 0;
+
   switch (type) {
     case 'contain':
-      return applyContainObjectFit(cw, ch, iw, ih);
+      return applyContainObjectFit(cw, ch, iw, ih, px, py);
     case 'cover':
       if (cr > ir) {
         const width = cw;
         const height = width / ir;
-        const yOffset = (ch - height) * 0.5;
-        return { width, height, yOffset, xOffset: 0 };
+        // const yOffset = (ch - height) * 0.5;
+        return { width, height, yOffset, xOffset };
       } else {
         const height = ch;
         const width = height * ir;
-        const xOffset = (cw - width) * 0.5;
-        return { width, height, xOffset, yOffset: 0 };
+        // const xOffset = (cw - width) * 0.5;
+        return { width, height, xOffset, yOffset };
       }
     case 'none':
-      return applyNoneObjectFit(cw, ch, iw, ih);
+      return applyNoneObjectFit(cw, ch, iw, ih, px, py);
     case 'scale-down':
-      const containDimension = applyContainObjectFit(cw, ch, iw, ih);
-      const noneDimension = applyNoneObjectFit(cw, ch, iw, ih);
+      const containDimension = applyContainObjectFit(cw, ch, iw, ih, px, py);
+      const noneDimension = applyNoneObjectFit(cw, ch, iw, ih, px, py);
 
       return containDimension.width < noneDimension.width
         ? containDimension
@@ -64,8 +63,8 @@ export const resolveObjectFit = (
       return {
         width: cw,
         height: ch,
-        xOffset: 0,
-        yOffset: 0,
+        xOffset,
+        yOffset,
       };
   }
 };

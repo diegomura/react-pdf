b58be53c9fe073104e1cf811d5b0855447062b5a
Andrew Hansen <arahan@uw.edu>
2019-01-29 09:41:29 -0800

    Fix(Text): Dont add additional x value for lines (#469)

--- tests/text.test.js
+++ tests/text.test.js
@@ -4,6 +4,12 @@ import Text from '../src/elements/Text';
 import TextInstance from '../src/elements/TextInstance';
 import root from './utils/dummyRoot';
 
+jest.mock('@textkit/pdf-renderer', () => () =>
+  class PDFRendererMock {
+    render() {}
+  },
+);
+
 let dummyRoot;
 
 describe('Text', () => {
@@ -27,5 +33,28 @@ describe('Text', () => {
 
     expect(text.layoutEngine.layout.mock.calls).toHaveLength(1);
     expect(text.layoutEngine.layout.mock.calls[0][0].string).toBe('');
+  });
+
+  test('Should render the same rect x value for clones', async () => {
+    const text = new Text(dummyRoot, {});
+    const textInstance = new TextInstance(dummyRoot, 'sometext');
+
+    text.appendChild(textInstance);
+
+    text.getAbsoluteLayout = () => ({ top: 20, left: 20 });
+    text.layoutText(100, 100);
+
+    await text.render();
+
+    const textRectX = text.lines[0].rect.x;
+
+    const clone = text.clone();
+
+    clone.layoutText(100, 100);
+    clone.getAbsoluteLayout = () => ({ top: 20, left: 20 });
+
+    await clone.render();
+
+    expect(clone.lines[0].rect.x).toEqual(textRectX);
   });
 });

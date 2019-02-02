23b89c2869f75f4f843522de5e348c2f92e87a67
Diego Muracciole <diegomuracciole@gmail.com>
2019-02-02 17:25:37 -0300

    Add object-fit support for images (#447)

---
+++ src/utils/objectFit.js
@@ -0,0 +1,101 @@
+import matchPercent from './matchPercent';
+
+const isNumeric = n => {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+const applyContainObjectFit = (cw, ch, iw, ih, px, py) => {
+  const cr = cw / ch;
+  const ir = iw / ih;
+
+  const pxp = matchPercent(px);
+  const pyp = matchPercent(py);
+  const pxv = pxp ? pxp.percent : 0.5;
+  const pyv = pyp ? pyp.percent : 0.5;
+
+  if (cr > ir) {
+    const height = ch;
+    const width = height * ir;
+    const yOffset = isNumeric(py) ? py : 0;
+    const xOffset = isNumeric(px) ? px : (cw - width) * pxv;
+
+    return { width, height, xOffset, yOffset };
+  } else {
+    const width = cw;
+    const height = width / ir;
+    const xOffset = isNumeric(px) ? px : 0;
+    const yOffset = isNumeric(py) ? py : (ch - height) * pyv;
+
+    return { width, height, yOffset, xOffset };
+  }
+};
+
+const applyNoneObjectFit = (cw, ch, iw, ih, px, py) => {
+  const width = iw;
+  const height = ih;
+  const pxp = matchPercent(px);
+  const pyp = matchPercent(py);
+  const pxv = pxp ? pxp.percent : 0.5;
+  const pyv = pyp ? pyp.percent : 0.5;
+  const xOffset = isNumeric(px) ? px : (cw - width) * pxv;
+  const yOffset = isNumeric(py) ? py : (ch - height) * pyv;
+
+  return { width, height, xOffset, yOffset };
+};
+
+const applyCoverObjectFit = (cw, ch, iw, ih, px, py) => {
+  const ir = iw / ih;
+  const cr = cw / ch;
+
+  const pxp = matchPercent(px);
+  const pyp = matchPercent(py);
+  const pxv = pxp ? pxp.percent : 0.5;
+  const pyv = pyp ? pyp.percent : 0.5;
+
+  if (cr > ir) {
+    const width = cw;
+    const height = width / ir;
+    const xOffset = isNumeric(px) ? px : 0;
+    const yOffset = isNumeric(py) ? py : (ch - height) * pyv;
+    return { width, height, yOffset, xOffset };
+  } else {
+    const height = ch;
+    const width = height * ir;
+    const xOffset = isNumeric(px) ? px : (cw - width) * pxv;
+    const yOffset = isNumeric(py) ? py : 0;
+    return { width, height, xOffset, yOffset };
+  }
+};
+
+const applyScaleDownObjectFit = (cw, ch, iw, ih, px, py) => {
+  const containDimension = applyContainObjectFit(cw, ch, iw, ih, px, py);
+  const noneDimension = applyNoneObjectFit(cw, ch, iw, ih, px, py);
+
+  return containDimension.width < noneDimension.width
+    ? containDimension
+    : noneDimension;
+};
+
+const applyFillObjectFit = (cw, ch, px, py) => {
+  return {
+    width: cw,
+    height: ch,
+    xOffset: matchPercent(px) ? 0 : px || 0,
+    yOffset: matchPercent(py) ? 0 : py || 0,
+  };
+};
+
+export const resolveObjectFit = (type = 'fill', cw, ch, iw, ih, px, py) => {
+  switch (type) {
+    case 'contain':
+      return applyContainObjectFit(cw, ch, iw, ih, px, py);
+    case 'cover':
+      return applyCoverObjectFit(cw, ch, iw, ih, px, py);
+    case 'none':
+      return applyNoneObjectFit(cw, ch, iw, ih, px, py);
+    case 'scale-down':
+      return applyScaleDownObjectFit(cw, ch, iw, ih, px, py);
+    default:
+      return applyFillObjectFit(cw, ch, px, py);
+  }
+};

788818aca2edb96d2d1b3d1021c29b4d0e430f5e
Diego Muracciole <diegomuracciole@gmail.com>
2019-02-13 13:03:38 -0300

    Setup jest-fetch-mock

--- tests/utils/setupTests.js
+++ tests/utils/setupTests.js
@@ -1,7 +1,10 @@
 /* eslint-disable */
-import raf from './polyfills';
+import './polyfills';
+
 import Enzyme from 'enzyme';
+import fetchMock from 'jest-fetch-mock';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+global.fetch = fetchMock;
 global.BROWSER = false;

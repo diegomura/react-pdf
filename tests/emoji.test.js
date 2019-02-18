dd2ba8d84210678905543db0806fe6ceab08066e
Diego Muracciole <diegomuracciole@gmail.com>
2019-02-18 16:10:14 -0300

    Fix emoji rendering for standard fonts (#495)

--- tests/emoji.test.js
+++ tests/emoji.test.js
@@ -1,9 +1,16 @@
-import * as emoji from '../src/utils/emoji';
 import Font from '../src/font';
+import * as emoji from '../src/utils/emoji';
 import * as imageUtils from '../src/utils/image';
 
 jest.mock('../src/font');
+
 describe('emoji utils', () => {
+  beforeEach(() => {
+    imageUtils.resolveImage = jest.fn(() =>
+      Promise.resolve({ data: 'image_data' }),
+    );
+  });
+
   describe('fetch emojis', () => {
     test('Should fetch the right emojis when they have a length > 1 and only 1 part', async () => {
       const testString = 'I ❤️ react-pdf. ✈️ and 🛰️ now render properly.';
@@ -13,7 +20,6 @@ describe('emoji utils', () => {
         url: 'https://twemoji.maxcdn.com/2/72x72/',
       }));
 
-      imageUtils.resolveImage = jest.fn(() => Promise.resolve());
       await emoji.fetchEmojis(testString);
 
       const expectedUrls = [
@@ -21,21 +27,24 @@ describe('emoji utils', () => {
         'https://twemoji.maxcdn.com/2/72x72/2708.png', // ✈️
         'https://twemoji.maxcdn.com/2/72x72/1f6f0.png', // 🛰️
       ];
+
       const calls = imageUtils.resolveImage.mock.calls;
-      expect(calls[0][0]).toEqual(expectedUrls[0]);
-      expect(calls[1][0]).toEqual(expectedUrls[1]);
-      expect(calls[2][0]).toEqual(expectedUrls[2]);
+      expect(calls[0][0]).toEqual({ uri: expectedUrls[0] });
+      expect(calls[1][0]).toEqual({ uri: expectedUrls[1] });
+      expect(calls[2][0]).toEqual({ uri: expectedUrls[2] });
     });
 
     test('Should fetch the right emoji, when it has two parts', async () => {
       const testString = 'This 👍🏿 should be properly fetched.';
-      imageUtils.resolveImage = jest.fn(() => Promise.resolve());
+
       await emoji.fetchEmojis(testString);
+
       const expectedUrls = [
         'https://twemoji.maxcdn.com/2/72x72/1f44d-1f3ff.png', // 👍🏿
       ];
+
       const calls = imageUtils.resolveImage.mock.calls;
-      expect(calls[0][0]).toEqual(expectedUrls[0]);
+      expect(calls[0][0]).toEqual({ uri: expectedUrls[0] });
     });
   });
 });

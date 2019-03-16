ffdd0bad2e5457c62c5ac7498e3e409db81fbc21
Kuzivakwashe <46272157+kuzivany@users.noreply.github.com>
2019-03-17 01:02:32 +0200

    Support multiple protocols for links (#515)

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,6 +1,8 @@
-const PROTOCOL_REGEXP = /^(http|https|ftp|ftps|mailto)/i;
+const PROTOCOL_REGEXP = /^([a-z]+\:(\/\/)?)/i;
 
 export const getURL = value => {
+  if (!value) return '';
+
   if (typeof value === 'string' && !value.match(PROTOCOL_REGEXP)) {
     return `http://${value}`;
   }

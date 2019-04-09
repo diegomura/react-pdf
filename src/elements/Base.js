0ebb28a92bce00a420a44691983a7afed4161867
Diego Muracciole <diegomuracciole@gmail.com>
2019-04-09 15:30:08 -0300

    Remove lodash dependencies (#555)

--- src/elements/Base.js
+++ src/elements/Base.js
@@ -1,7 +1,4 @@
-import pick from 'lodash.pick';
-import merge from 'lodash.merge';
-import toPairsIn from 'lodash.topairsin';
-import isFunction from 'lodash.isfunction';
+import { pick, toPairsIn } from 'ramda';
 
 import Node from './Node';
 import StyleSheet from '../stylesheet';
@@ -10,7 +7,9 @@ import Borders from '../mixins/borders';
 import Clipping from '../mixins/clipping';
 import Transform from '../mixins/transform';
 import warning from '../utils/warning';
+import deepMerge from '../utils/deepMerge';
 import upperFirst from '../utils/upperFirst';
+import isFunction from '../utils/isFunction';
 import matchPercent from '../utils/matchPercent';
 import { inheritedProperties } from '../stylesheet/inherit';
 
@@ -20,12 +19,11 @@ class Base extends Node {
 
     this.root = root;
     this.style = {};
-    this.props = merge(
-      {},
+    this.props = deepMerge([
       this.constructor.defaultProps,
       Base.defaultProps,
       props,
-    );
+    ]);
 
     warning(!this.props.styles, '"styles" prop passed instead of "style" prop');
   }
@@ -87,12 +85,11 @@ class Base extends Node {
   }
 
   update(newProps) {
-    this.props = merge(
-      {},
+    this.props = deepMerge([
       this.constructor.defaultProps,
       Base.defaultProps,
       newProps,
-    );
+    ]);
     this.root.markDirty();
   }
 
@@ -118,7 +115,7 @@ class Base extends Node {
     });
 
     const inheritedStyles = this.parent
-      ? pick(this.parent.style, inheritedProperties)
+      ? pick(inheritedProperties, this.parent.style)
       : {};
 
     return { ...inheritedStyles, ...ownStyles };

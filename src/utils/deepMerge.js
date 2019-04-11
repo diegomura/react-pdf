363decedd2b2ded494ed1159cf95de46cdd4f5cf
Diego Muracciole <diegomuracciole@gmail.com>
2019-04-11 13:11:43 -0300

    Gracefully handle undefined styles

--- src/utils/deepMerge.js
+++ src/utils/deepMerge.js
@@ -1,8 +1,12 @@
-import { mergeDeepRight } from 'ramda';
+import { isNil, mergeDeepWith } from 'ramda';
+
+const merge = (a, b) => {
+  return isNil(b) ? a : b;
+};
 
 const deepMerge = objs =>
   objs.reduce((acc, obj) => {
-    return mergeDeepRight(acc, obj);
+    return mergeDeepWith(merge, acc, obj);
   }, {});
 
 export default deepMerge;

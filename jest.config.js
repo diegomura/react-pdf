670bba84e37d0e8a28d44f6225480703c109b5cb
Diego Muracciole <diegomuracciole@gmail.com>
2019-06-03 01:30:03 -0300

    Generate test coverage reports

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   testRegex: 'tests/.*?(test)\\.js$',
   setupFiles: ['<rootDir>tests/internal/setupTests.js'],
+  coverageDirectory: './coverage/',
+  collectCoverage: true,
   moduleNameMapper: {
     'cross-fetch': 'jest-fetch-mock',
     'yoga-layout': 'yoga-layout-prebuilt',

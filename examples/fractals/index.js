9f3dde43ab7dff1485ce567c8e1587d65b1d40b5
- Pocko <Ruslan_Nikolov@epam.com>
2019-10-16 10:14:03 +0300

    .

--- examples/fractals/index.js
+++ examples/fractals/index.js
@@ -3,29 +3,30 @@
 import React from 'react';
 
 import ReactPDF, {
-  Page,
-  Document,
-  Link,
-  View,
-  Text,
+    Page,
+    Document,
+    Link,
+    View,
+    Text,
 } from '../../dist/react-pdf.es.js';
 
 const doc = (
-  <Document
-    title="Fractals"
-    author="John Doe"
-    subject="Rendering fractals with react-pdf"
-    keywords={['react', 'pdf', 'fractals']}
-  >
-    <Page size="A4">
-      <Link src="-myDist">Link</Link>
-    </Page>
-    <Page size="A4">
-      <View dest="-myDist">
-        <Text>Hello</Text>
-      </View>
-    </Page>
-  </Document>
+    <Document
+        title="Fractals"
+        author="John Doe"
+        subject="Rendering fractals with react-pdf"
+        keywords={['react', 'pdf', 'fractals']}
+    >
+        <Page size="A4">
+            <Link src="-myDist">Link</Link>
+        </Page>
+
+        <Page size="A4">
+            <View dest="-myDist">
+                <Text>Hello</Text>
+            </View>
+        </Page>
+    </Document>
 );
 
 ReactPDF.render(doc, `${__dirname}/output.pdf`);

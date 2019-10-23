906066d3c4e15a04347c85dd67311fe73d72b585
RuslanNikolov <Ruslan_Nikolov@epam.com>
2019-10-23 14:09:50 +0300

    added first change

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,11 +1,24 @@
 const PROTOCOL_REGEXP = /^([a-z]+\:(\/\/)?)/i;
+const DEST_REGEXP = /^#.+/;
 
 export const getURL = value => {
   if (!value) return '';
+
+  if (isSrcDest(value)) return value; // don't modify it if it is a destination
 
   if (typeof value === 'string' && !value.match(PROTOCOL_REGEXP)) {
     return `http://${value}`;
   }
 
   return value;
+};
+
+export const isSrcDest = src => src.match(DEST_REGEXP);
+
+export const setLink = node => {
+  const { top, left, width, height } = node.getAbsoluteLayout();
+  const instanceMethod = isSrcDest(node.src) ? 'goTo' : 'link';
+  const nodeSrc = isSrcDest(node.src) ? node.src.slice(1) : node.src;
+
+  node.root.instance[instanceMethod](left, top, width, height, nodeSrc);
 };

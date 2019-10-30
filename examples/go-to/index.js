c9ccc2d0a26288fc662da0698d5c8449c74ec702
RuslanNikolov <Ruslan_Nikolov@epam.com>
2019-10-30 14:24:21 +0200

    fixed the dependency

---
+++ examples/go-to/index.js
@@ -0,0 +1,29 @@
+/* eslint react/prop-types: 0, react/jsx-sort-props: 0 */
+
+import React from 'react';
+
+import ReactPDF, {
+  Page,
+  Document,
+  Image,
+  Link,
+  View,
+} from '../../dist/react-pdf.es.js';
+
+const doc = (
+  <Document>
+    <Page size="A4">
+      <Link href="#myDest">Link</Link>
+    </Page>
+
+    <Page size="A4">
+      <View style={{ height: 300 }} />
+      <Image
+        destination="myDest"
+        src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg"
+      />
+    </Page>
+  </Document>
+);
+
+ReactPDF.render(doc, `${__dirname}/output.pdf`);

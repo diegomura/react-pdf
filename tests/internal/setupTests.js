6423351e039a9ce0e983f0aa900095e91a68ae79
Diego Muracciole <diegomuracciole@gmail.com>
2019-11-13 01:41:23 -0300

    Add react and react-dom as dev dependencies for testing purposes

--- tests/internal/setupTests.js
+++ tests/internal/setupTests.js
@@ -1,9 +1,9 @@
 /* eslint-disable */
 import './polyfills';
 
-// import Enzyme from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-// Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
 global.BROWSER = false;
 global.fetch = require('jest-fetch-mock');

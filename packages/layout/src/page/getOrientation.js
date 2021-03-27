7f5bcbe362542e9c2526bda6d0c1c20043a9e8e1
Diego Muracciole <diegomuracciole@gmail.com>
2021-03-27 13:17:05 -0300

    Fix TS and comments

--- packages/layout/src/page/getOrientation.js
+++ packages/layout/src/page/getOrientation.js
@@ -3,7 +3,7 @@ import * as R from 'ramda';
 const VALID_ORIENTATIONS = ['portrait', 'landscape'];
 
 /**
- * Get page orientation. Defaults to landscape
+ * Get page orientation. Defaults to portrait
  *
  * @param { Object } page object
  * @returns { String } page orientation
--- packages/types/style.d.ts
+++ packages/types/style.d.ts
@@ -1,116 +1,117 @@
 export interface Style {
   //Flexbox
 
-  alignContent?: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'space-between' | 'space-around',
-  alignItems?: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline',
-  alignSelf?: 'auto' | 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch',
-  flex?: number,
-  flexDirection?: 'row' | 'row-reverse' | 'column' | 'column-reverse',
-  flexWrap?: 'nowrap' | 'wrap' | 'wrap-reverse',
-  flexFlow?: number,
-  flexGrow?: number,
-  flexShrink?: number,
-  flexBasis?: number,
-  justifyContent?: 'flex-start' | 'flex-end' | 'center' | 'space-around' | 'space-between' | 'space-evenly',
-  order?: number,
+  alignContent?: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'space-between' | 'space-around';
+  alignItems?: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline';
+  alignSelf?: 'auto' | 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch';
+  flex?: number;
+  flexDirection?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
+  flexWrap?: 'nowrap' | 'wrap' | 'wrap-reverse';
+  flexFlow?: number;
+  flexGrow?: number;
+  flexShrink?: number;
+  flexBasis?: number;
+  justifyContent?: 'flex-start' | 'flex-end' | 'center' | 'space-around' | 'space-between' | 'space-evenly';
+  order?: number;
 
-  // Layout?:never,
+  // Layout
 
-  bottom?: number | string,
-  display?: 'flex' | 'none',
-  left?: number | string,
-  position?: 'absolute' | 'relative',
-  right?: number | string,
-  top?: number | string,
+  bottom?: number | string;
+  display?: 'flex' | 'none';
+  left?: number | string;
+  position?: 'absolute' | 'relative';
+  right?: number | string;
+  top?: number | string;
+  overflow?: 'hidden';
 
-  // Dimension?:never,
+  // Dimension
 
-  height?: number | string,
-  maxHeight?: number | string,
-  maxWidth?: number | string,
-  minHeight?: number | string,
-  minWidth?: number | string,
-  width?: number | string,
+  height?: number | string;
+  maxHeight?: number | string;
+  maxWidth?: number | string;
+  minHeight?: number | string;
+  minWidth?: number | string;
+  width?: number | string;
 
-  // Color?:never,
+  // Color
 
-  backgroundColor?: string,
-  color?: string,
-  opacity?: number,
+  backgroundColor?: string;
+  color?: string;
+  opacity?: number;
 
-  // Text?:never,
+  // Text
 
-  fontSize?: number | string,
-  fontFamily?: string,
-  fontStyle?: string | 'normal',
-  fontWeight?: number | 'thin' | 'hairline' | 'ultralight' | 'extralight' | 'light' | 'normal' | 'medium' | 'semibold' | 'demibold' | 'bold' | 'ultrabold' | 'extrabold' | 'heavy' | 'black',
-  letterSpacing?: number | string,
-  lineHeight?: number | string,
-  maxLines?: number, //?
-  textAlign?: 'left' | 'right' | 'center' | 'justify', //?
-  textDecoration?: 'line-through' | 'underline' | 'none',
-  textDecorationColor?: string,
-  textDecorationStyle?: "dashed" | "dotted" | "solid" | string, //?
-  textIndent?: any, //?
-  textOverflow?: any, //?
-  textTransform?: 'capitalize' | 'lowercase' | 'uppercase',
+  fontSize?: number | string;
+  fontFamily?: string;
+  fontStyle?: string | 'normal';
+  fontWeight?: number | 'thin' | 'hairline' | 'ultralight' | 'extralight' | 'light' | 'normal' | 'medium' | 'semibold' | 'demibold' | 'bold' | 'ultrabold' | 'extrabold' | 'heavy' | 'black';
+  letterSpacing?: number | string;
+  lineHeight?: number | string;
+  maxLines?: number; //?
+  textAlign?: 'left' | 'right' | 'center' | 'justify'; //?
+  textDecoration?: 'line-through' | 'underline' | 'none';
+  textDecorationColor?: string;
+  textDecorationStyle?: "dashed" | "dotted" | "solid" | string; //?
+  textIndent?: any; //?
+  textOverflow?: 'ellipsis';
+  textTransform?: 'capitalize' | 'lowercase' | 'uppercase';
 
-  // Sizing/positioning?:never,
+  // Sizing/positioning
 
-  objectFit?: string,
-  objectPosition?: number | string,
-  objectPositionX?: number | string,
-  objectPositionY?: number | string,
+  objectFit?: string;
+  objectPosition?: number | string;
+  objectPositionX?: number | string;
+  objectPositionY?: number | string;
 
-  // Margin/padding?:never,
+  // Margin/padding
 
-  margin?: number | string,
-  marginHorizontal?: number | string,
-  marginVertical?: number | string,
-  marginTop?: number | string,
-  marginRight?: number | string,
-  marginBottom?: number | string,
-  marginLeft?: number | string,
-  padding?: number | string,
-  paddingHorizontal?: number | string,
-  paddingVertical?: number | string,
-  paddingTop?: number | string,
-  paddingRight?: number | string,
-  paddingBottom?: number | string,
-  paddingLeft?: number | string,
+  margin?: number | string;
+  marginHorizontal?: number | string;
+  marginVertical?: number | string;
+  marginTop?: number | string;
+  marginRight?: number | string;
+  marginBottom?: number | string;
+  marginLeft?: number | string;
+  padding?: number | string;
+  paddingHorizontal?: number | string;
+  paddingVertical?: number | string;
+  paddingTop?: number | string;
+  paddingRight?: number | string;
+  paddingBottom?: number | string;
+  paddingLeft?: number | string;
 
-  // Transformations?:never,
+  // Transformations
 
-  transform?: string,
-  transformOrigin?: number | string,
-  transformOriginX?: number | string,
-  transformOriginY?: number | string,
+  transform?: string;
+  transformOrigin?: number | string;
+  transformOriginX?: number | string;
+  transformOriginY?: number | string;
 
-  // Borders?:never,
+  // Borders
 
-  border?: number | string,
-  borderWidth?: number,
-  borderColor?: string,
-  borderStyle?: "dashed" | "dotted" | "solid",
-  borderTop?: number | string,
-  borderTopColor?: string,
-  borderTopStyle?: "dashed" | "dotted" | "solid", // ?
-  borderTopWidth?: number | string,
-  borderRight?: number | string,
-  borderRightColor?: string,
-  borderRightStyle?: "dashed" | "dotted" | "solid", //?
-  borderRightWidth?: number | string,
-  borderBottom?: number | string,
-  borderBottomColor?: string,
-  borderBottomStyle?: "dashed" | "dotted" | "solid", //?
-  borderBottomWidth?: number | string,
-  borderLeft?: number | string,
-  borderLeftColor?: string,
-  borderLeftStyle?: "dashed" | "dotted" | "solid", //?
-  borderLeftWidth?: number | string,
-  borderTopLeftRadius?: number | string,
-  borderTopRightRadius?: number | string,
-  borderBottomRightRadius?: number | string,
-  borderBottomLeftRadius?: number | string,
+  border?: number | string;
+  borderWidth?: number;
+  borderColor?: string;
+  borderStyle?: "dashed" | "dotted" | "solid";
+  borderTop?: number | string;
+  borderTopColor?: string;
+  borderTopStyle?: "dashed" | "dotted" | "solid"; // ?
+  borderTopWidth?: number | string;
+  borderRight?: number | string;
+  borderRightColor?: string;
+  borderRightStyle?: "dashed" | "dotted" | "solid"; //?
+  borderRightWidth?: number | string;
+  borderBottom?: number | string;
+  borderBottomColor?: string;
+  borderBottomStyle?: "dashed" | "dotted" | "solid"; //?
+  borderBottomWidth?: number | string;
+  borderLeft?: number | string;
+  borderLeftColor?: string;
+  borderLeftStyle?: "dashed" | "dotted" | "solid"; //?
+  borderLeftWidth?: number | string;
+  borderTopLeftRadius?: number | string;
+  borderTopRightRadius?: number | string;
+  borderBottomRightRadius?: number | string;
+  borderBottomLeftRadius?: number | string;
   borderRadius?: number | string
 }

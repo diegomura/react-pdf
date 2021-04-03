f616ad581cd29086cf459e673a5b1a38d25f686a
Diego Muracciole <diegomuracciole@gmail.com>
2021-04-03 18:25:05 -0300

    Fix rgba color support

--- packages/render/src/primitives/renderBackground.js
+++ packages/render/src/primitives/renderBackground.js
@@ -1,17 +1,29 @@
 import * as R from 'ramda';
+import colorString from 'color-string';
 
 import save from '../operations/save';
 import restore from '../operations/restore';
 import clipNode from '../operations/clipNode';
 
-const drawBackground = ctx => node => {
+const parseColor = hex => {
+  const parsed = colorString.get(hex);
+  const value = colorString.to.hex(parsed.value.slice(0, 3));
+  const opacity = parsed.value[3];
+
+  return { value, opacity };
+};
+
+const drawBackground = (ctx, node) => {
   if (node.box && node.style.backgroundColor) {
     const { top, left, width, height } = node.box;
-    const opacity = R.defaultTo(1, node.style?.opacity);
+    const color = parseColor(node.style.backgroundColor);
+    const opacity = R.isNil(color.opacity)
+      ? R.defaultTo(1, node.style?.opacity)
+      : color.opacity;
 
     ctx
       .fillOpacity(opacity)
-      .fillColor(node.style.backgroundColor)
+      .fillColor(color.value)
       .rect(left, top, width, height)
       .fill();
   }
@@ -19,21 +31,15 @@ const drawBackground = ctx => node => {
   return node;
 };
 
-const shouldRenderBackground = R.both(
-  R.has('box'),
-  R.hasPath(['style', 'backgroundColor']),
-);
-
 const renderBackground = (ctx, node) => {
-  R.when(
-    shouldRenderBackground,
-    R.compose(
-      restore(ctx),
-      drawBackground(ctx),
-      clipNode(ctx),
-      save(ctx),
-    ),
-  )(node);
+  const hasBackground = !!node.box && !!node.style?.backgroundColor;
+
+  if (hasBackground) {
+    save(ctx, node);
+    clipNode(ctx, node);
+    drawBackground(ctx, node);
+    restore(ctx, node);
+  }
 
   return node;
 };

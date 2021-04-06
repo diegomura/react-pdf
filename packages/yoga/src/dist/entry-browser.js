8c06f3316fa67021fcba4487d0345a3cf72e6541
Diego Muracciole <diegomuracciole@gmail.com>
2021-04-06 16:32:55 -0300

    Init yoga with correct memory heap

--- packages/yoga/src/dist/entry-browser.js
+++ packages/yoga/src/dist/entry-browser.js
@@ -8,7 +8,7 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
- * 
+ *
  * @format
  */
 
@@ -18,7 +18,41 @@ var nbind = require('../build/Release/nbind.js');
 var ran = false;
 var ret = null;
 
-nbind({}, function (err, result) {
+var isBrowser = typeof window !== 'undefined';
+
+var DEFAULT_TOTAL_MEMORY = 1073741824;
+
+function roundBy2(value) {
+  return value;
+}
+
+function getBrowserTotalMemory() {
+  // Chrome
+  // https://developer.mozilla.org/en-US/docs/Web/API/Performance/memory
+  if (typeof window.performance !== 'undefined' &&
+    typeof window.performance.memory !== 'undefined' &&
+    typeof window.performance.memory.jsHeapSizeLimit === 'number') {
+    return window.performance.memory.jsHeapSizeLimit
+  }
+
+  return null;
+}
+
+function getTotalMemory() {
+  const configuredTotalMemory = process.env.TEST_MEMORY;
+  if (configuredTotalMemory) return roundBy2(configuredTotalMemory)
+
+  if (isBrowser) {
+    const totalMemory = getBrowserTotalMemory()
+    if (totalMemory) return roundBy2(totalMemory)
+  }
+
+  return DEFAULT_TOTAL_MEMORY
+}
+
+var totalMemory = Math.min(DEFAULT_TOTAL_MEMORY, getTotalMemory());
+
+nbind({ TOTAL_MEMORY: totalMemory }, function (err, result) {
   if (ran) {
     return;
   }
---
+++ test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function isNode() {
+  return ;
+}
+
+console.log(isNode());
+
+console.log(typeof hegerg === 'undefined');

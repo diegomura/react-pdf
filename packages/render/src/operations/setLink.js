b7e59d9630dd362c51f1474e84a159cb597be1a0
Diego Muracciole <diegomuracciole@gmail.com>
2021-04-08 18:11:35 -0300

    Fix nested Link

--- packages/render/src/operations/setLink.js
+++ packages/render/src/operations/setLink.js
@@ -6,16 +6,19 @@ const DEST_REGEXP = /^#.+/;
 
 const isSrcId = R.test(DEST_REGEXP);
 
-const getSource = R.compose(
-  R.either(R.path(['props', 'src']), R.path(['props', 'href'])),
-);
+const getSource = node => {
+  const props = node.props || {};
+  return props.src || props.href;
+};
 
 const setLink = (ctx, node) => {
   const { top, left, width, height } = node.box;
   const src = getSource(node);
 
   if (src) {
-    const [instanceMethod, value] = isSrcId(src) ? ['goTo', src.slice(1)] : ['link', src];
+    const [instanceMethod, value] = isSrcId(src)
+      ? ['goTo', src.slice(1)]
+      : ['link', src];
     ctx[instanceMethod](left, top, width, height, value);
   }
 

2aae9fcde81700854abd7f65d00e23615ac71390
diegomura <diegomuracciole@gmail.com>
2021-04-24 22:35:48 -0300

    Remove new Buffer from yoga build

--- packages/yoga/postbuild.js
+++ packages/yoga/postbuild.js
@@ -23,12 +23,19 @@ function replace(buffer, target, dst) {
 // Increase memory heap from 128 MB to 256 MB
 nbindBuffer = replace(nbindBuffer, '134217728', '268435456');
 
-// // Fixes yoga nbind issue
-// // https://github.com/vadimdemedes/yoga-layout-prebuilt/issues/2
+// Fixes yoga nbind issue
+// https://github.com/vadimdemedes/yoga-layout-prebuilt/issues/2
 nbindBuffer = replace(
   nbindBuffer,
   '_a = _typeModule(_typeModule),',
   'var _a = _typeModule(_typeModule);',
+);
+
+// Replaces buffer creation
+nbindBuffer = replace(
+  nbindBuffer,
+  'new Buffer',
+  'Buffer.from',
 );
 
 fs.writeFileSync(nbindPath, nbindBuffer);

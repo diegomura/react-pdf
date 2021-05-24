5999b7950d7bc6fc2cd7057570a0ed4536425baa
byeokim <29354589+byeokim@users.noreply.github.com>
2021-05-24 15:07:43 +0900

    feat: error handling when source factories of Image return undefined

--- packages/layout/src/image/fetchImage.js
+++ packages/layout/src/image/fetchImage.js
@@ -22,6 +22,11 @@ const fetchImage = async node => {
 
   try {
     const source = await resolveSource(src);
+
+    if (!source) {
+      throw new Error(`Image's "src" or "source" prop returned ${source}`);
+    }
+
     node.image = await resolveImage(source, { cache });
   } catch (e) {
     node.image = { width: 0, height: 0 };

5cd94dfe38009a3db974a38d20408b3fd183ab66
jeetiss <jeetiss@yandex.ru>
2021-06-02 17:26:24 +0300

    rework transform

--- packages/render/src/operations/transform.js
+++ packages/render/src/operations/transform.js
@@ -2,113 +2,133 @@ import * as R from 'ramda';
 
 // TODO: Implement using only matrices to support skew and even more operations than css.
 
-const getRotation = transform => {
-  const match = /rotate\((-?\d+(.\d+)?)(.+)\)/g.exec(transform);
-
-  if (match && match[1] && match[3]) {
-    const value = match[1];
-    return match[3] === 'rad' ? (value * 180) / Math.PI : value;
+const parse = transformString => {
+  const transforms = transformString.trim().split(/\) |\)/);
+  // Handle "initial", "inherit", "unset".
+  if (transforms.length === 1) {
+    return [[transforms[0], true]];
   }
 
-  return 0;
-};
+  const parsed = [];
 
-const getTranslateX = transform => {
-  const matchX = /translateX\((-?\d+\.?d*)\)/g.exec(transform);
-  const matchGeneric = /translate\((-?\d+\.?d*).*(,|\s)\s*(-?\d+\.?d*).*\)/g.exec(
-    transform,
-  );
+  for (let i = 0; i < transforms.length; i += 1) {
+    const transform = transforms[i];
 
-  if (matchX && matchX[1]) return matchX[1];
-  if (matchGeneric && matchGeneric[1]) return matchGeneric[1];
-
-  return 0;
-};
-
-const getTranslateY = transform => {
-  const matchY = /translateY\((-?\d+\.?\d*)\)/g.exec(transform);
-  const matchGeneric = /translate\((-?\d+\.?\d*).*(,|\s)\s*(-?\d+\.?\d*).*\)/g.exec(
-    transform,
-  );
-
-  if (matchY && matchY[1]) return matchY[1];
-  if (matchGeneric && matchGeneric[3]) return matchGeneric[3];
-
-  return 0;
-};
-
-const getScaleX = transform => {
-  const matchX = /scaleX\((-?\d+\.?\d*)\)/g.exec(transform);
-  const matchGeneric = /scale\((-?\d+\.?\d*).*,?\s*(-?\d+\.?\d*).*\)/g.exec(
-    transform,
-  );
-
-  if (matchX && matchX[1]) return matchX[1];
-  if (matchGeneric && matchGeneric[1]) return matchGeneric[1];
-
-  return 1;
-};
-
-const getScaleY = transform => {
-  const matchY = /scaleY\((-?\d+\.?\d*)\)/g.exec(transform);
-  const matchGeneric = /scale\((-?\d+\.?\d*).*,?\s*(-?\d+\.?\d*).*\)/g.exec(
-    transform,
-  );
-
-  if (matchY && matchY[1]) return matchY[1];
-  if (matchGeneric && matchGeneric[2]) return matchGeneric[2];
-
-  return 1;
-};
-
-const getMatrix = transform => {
-  const match = /matrix\(([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+)\)/g.exec(
-    transform,
-  );
-  if (match) return match.slice(1, 7);
-  return null;
-};
-
-const applySingleTransformation = (ctx, transform, origin) => {
-  if (/rotate/g.test(transform)) {
-    ctx.rotate(getRotation(transform), { origin });
-  } else if (/scaleX/g.test(transform)) {
-    ctx.scale(getScaleX(transform), 1, { origin });
-  } else if (/scaleY/g.test(transform)) {
-    ctx.scale(1, getScaleY(transform), { origin });
-  } else if (/scale/g.test(transform)) {
-    ctx.scale(getScaleX(transform), getScaleY(transform), {
-      origin,
-    });
-  } else if (/translateX/g.test(transform)) {
-    ctx.translate(getTranslateX(transform), 1, { origin });
-  } else if (/translateY/g.test(transform)) {
-    ctx.translate(1, getTranslateY(transform), { origin });
-  } else if (/translate/g.test(transform)) {
-    ctx.translate(getTranslateX(transform), getTranslateY(transform), {
-      origin,
-    });
-  } else if (/matrix/g.test(transform)) {
-    ctx.transform(...getMatrix(transform));
+    if (transform) {
+      const [operationName, operationValueRaw] = transform.split('(');
+      const values = operationValueRaw.split(',').map(val => val.trim());
+      const value = values.length === 1 ? values[0] : values;
+      parsed.push([operationName, value]);
+    }
   }
+
+  return parsed;
+};
+
+const applySingleTransformation = (ctx, operation, origin) => {
+  const [name, args] = operation;
+
+  switch (name) {
+    case 'scale': {
+      const [scaleX, scaleY] = args;
+      ctx.scale(scaleX, scaleY, { origin });
+      break;
+    }
+
+    case 'rotate': {
+      ctx.rotate(args, { origin });
+      break;
+    }
+
+    case 'translate': {
+      const [x, y] = args;
+      ctx.translate(x, y, { origin });
+      break;
+    }
+
+    case 'matrix': {
+      ctx.transform(...args);
+      break;
+    }
+
+    default: {
+      console.error(`Transform operation ${name} doesn't supported`);
+    }
+  }
+};
+
+const normalizeOperation = (operation, args) => {
+  switch (operation) {
+    case 'scale': {
+      const isArray = Array.isArray(args);
+      const parsed = isArray
+        ? [Number.parseFloat(args[0]), Number.parseFloat(args[1])]
+        : Number.parseFloat(args);
+      const [scaleX, scaleY] = isArray ? parsed : [parsed, parsed];
+      return ['scale', [scaleX, scaleY]];
+    }
+
+    case 'scaleX': {
+      return ['scale', [Number.parseFloat(args), 1]];
+    }
+    case 'scaleY': {
+      return ['scale', [1, Number.parseFloat(args)]];
+    }
+
+    case 'rotate': {
+      const unitsRegexp = /(-?\d*\.?\d*)(\w*)?/i;
+      const [, value, unit] = unitsRegexp.exec(args);
+      const number = Number.parseFloat(value);
+
+      return ['rotate', unit === 'rad' ? (number * 180) / Math.PI : number];
+    }
+
+    case 'translate': {
+      return [
+        'translate',
+        [Number.parseFloat(args[0]), Number.parseFloat(args[1])],
+      ];
+    }
+
+    case 'translateX': {
+      return ['translate', [Number.parseFloat(args), 0]];
+    }
+
+    case 'translateY': {
+      return ['translate', [0, Number.parseFloat(args)]];
+    }
+
+    default: {
+      return [operation, args.map(num => Number.parseFloat(num))];
+    }
+  }
+};
+
+const normalize = operations => {
+  const normalized = [];
+  operations.forEach(([operation, args]) => {
+    normalized.push(normalizeOperation(operation, args));
+  });
+
+  return normalized;
 };
 
 const applyTransformations = (ctx, node) => {
   if (!node.origin) return node;
 
-  let match;
-  const re = /[a-zA-Z]+\([^)]+\)/g;
   const origin = [node.origin.left, node.origin.top];
   const transform =
     (node.style && node.style.transform) ||
     (node.props && node.props.transform) ||
     '';
+  const operations = normalize(parse(transform));
 
-  while ((match = re.exec(transform)) != null) {
-    applySingleTransformation(ctx, match[0], origin);
-  }
+  operations.forEach(operation => {
+    applySingleTransformation(ctx, operation, origin);
+  });
 
   return node;
 };
 
+export { parse as parseTransform, normalize as normalizeTransform };
 export default R.curryN(2, applyTransformations);

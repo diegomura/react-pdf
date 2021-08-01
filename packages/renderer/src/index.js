aa5a438c75e84c644c7139823c059eca3e65d5d1
Diego Muracciole <diegomuracciole@gmail.com>
2021-08-01 19:47:50 -0300

    fix: revert refactor (#1436)

--- packages/renderer/src/index.js
+++ packages/renderer/src/index.js
@@ -12,11 +12,11 @@ const fontStore = new FontStore();
 // We must keep a single renderer instance, otherwise React will complain
 let renderer;
 
-const pdf = initialValue => {
-  // The pdf instance acts as an event emitter for DOM usage.
-  // We only want to trigger an update when PDF content changes
-  const events = {};
+// The pdf instance acts as an event emitter for DOM usage.
+// We only want to trigger an update when PDF content changes
+const events = {};
 
+const pdf = initialValue => {
   const onChange = () => {
     const listeners = events.change?.slice() || [];
     for (let i = 0; i < listeners.length; i += 1) listeners[i]();

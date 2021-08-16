1c90fc96eebe62263f10fad04056deb784a615f3
Michael Arthur <michael@jumblesoft.co.nz>
2021-08-17 11:00:20 +1200

    Update borderWidth type to accept string

--- packages/types/style.d.ts
+++ packages/types/style.d.ts
@@ -89,7 +89,7 @@ export interface Style {
   // Borders
 
   border?: number | string;
-  borderWidth?: number;
+  borderWidth?: number | string;
   borderColor?: string;
   borderStyle?: "dashed" | "dotted" | "solid";
   borderTop?: number | string;

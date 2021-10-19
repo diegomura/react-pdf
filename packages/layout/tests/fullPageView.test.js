7acd39fa1b60d2379e48584964d8c4643aa473be
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2021-10-19 15:00:08 +0300

    fix: full page blocks (#1541)

---
+++ packages/layout/tests/fullPageView.test.js
@@ -0,0 +1,56 @@
+import resolvePagination from '../src/steps/resolvePagination';
+import resolveDimensions from '../src/steps/resolveDimensions';
+
+const calcLayout = node => resolvePagination(resolveDimensions(node));
+
+describe('layout', () => {
+  const root = {
+    type: 'DOCUMENT',
+    children: [
+      {
+        type: 'PAGE',
+        box: {},
+        style: {
+          width: 100,
+          height: 100,
+        },
+        children: [
+          {
+            type: 'VIEW',
+            box: {},
+            style: {
+              position: 'absolute',
+              width: '50%',
+              top: 0,
+              bottom: 0,
+            },
+            props: {},
+            children: [],
+          },
+          {
+            type: 'TEXT',
+            box: {},
+            style: {},
+            props: {},
+            children: [
+              {
+                type: 'TEXT_INSTANCE',
+                value: 'hello world',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
+  test('should stretch absolute block to full page size', () => {
+    const layout = calcLayout(root);
+
+    const page = layout.children[0];
+    const view = layout.children[0].children[0];
+
+    expect(page.box.height).toBe(100);
+    expect(view.box.height).toBe(100);
+  });
+});

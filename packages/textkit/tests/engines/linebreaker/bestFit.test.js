e845b189ac6c64e5a3b2e0f079b60c1013c6e10a
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2021-12-18 20:46:32 +0300

    rewrite test

--- packages/textkit/tests/engines/linebreaker/bestFit.test.js
+++ packages/textkit/tests/engines/linebreaker/bestFit.test.js
@@ -1,214 +1,72 @@
 import applyBestFit from '../../../src/engines/linebreaker/bestFit';
 
-// Sentence: "1234567891011"
-const node1 = [
-  {
-    type: 'box',
-    width: 84.81599999999999,
-  },
-  {
-    type: 'glue',
-    width: 0,
-    stretch: 10000,
-    shrink: 0,
-  },
-  {
-    type: 'penalty',
-    width: 0,
-    penalty: -10000,
-  },
-];
-
-// Sentence: "123 1234567891011"
-const node2 = [
-  {
-    type: 'box',
-    width: 21.204,
-  },
-  {
-    type: 'glue',
-    width: 2.724,
-    stretch: 1.362,
-    shrink: 0.908,
-  },
-  {
-    type: 'box',
-    width: 91.88399999999999,
-  },
-  {
-    type: 'glue',
-    width: 0,
-    stretch: 10000,
-    shrink: 0,
-  },
-  {
-    type: 'penalty',
-    width: 0,
-    penalty: -10000,
-  },
-];
-
-// Setence: "123 1234567891011 123"
-const node3 = [
-  {
-    type: 'box',
-    width: 21.204,
-  },
-  {
-    type: 'glue',
-    width: 2.724,
-    stretch: 1.362,
-    shrink: 0.908,
-  },
-  {
-    type: 'box',
-    width: 91.88399999999999,
-  },
-  {
-    type: 'glue',
-    width: 2.724,
-    stretch: 1.362,
-    shrink: 0.908,
-  },
-  {
-    type: 'box',
-    width: 21.204,
-  },
-  {
-    type: 'glue',
-    width: 0,
-    stretch: 10000,
-    shrink: 0,
-  },
-  {
-    type: 'penalty',
-    width: 0,
-    penalty: -10000,
-  },
-];
-
-// Sentence: "123 1234567891011 123 1234567891011"
-const node4 = [
-  {
-    type: 'box',
-    width: 21.204,
-  },
-  {
-    type: 'glue',
-    width: 2.724,
-    stretch: 1.362,
-    shrink: 0.908,
-  },
-  {
-    type: 'box',
-    width: 91.88399999999999,
-  },
-  {
-    type: 'glue',
-    width: 2.724,
-    stretch: 1.362,
-    shrink: 0.908,
-  },
-  {
-    type: 'box',
-    width: 21.204,
-  },
-  {
-    type: 'glue',
-    width: 2.724,
-    stretch: 1.362,
-    shrink: 0.908,
-  },
-  {
-    type: 'box',
-    width: 91.88399999999999,
-  },
-  {
-    type: 'glue',
-    width: 0,
-    stretch: 10000,
-    shrink: 0,
-  },
-  {
-    type: 'penalty',
-    width: 0,
-    penalty: -10000,
-  },
-];
-
-// Sentence: "123 1234567891011 1234567891011 1234567891011"
-const node5 = [
-  {
-    type: 'box',
-    width: 21.204,
-  },
-  {
-    type: 'glue',
-    width: 2.724,
-    stretch: 1.362,
-    shrink: 0.908,
-  },
-  {
-    type: 'box',
-    width: 91.88399999999999,
-  },
-  {
-    type: 'glue',
-    width: 2.724,
-    stretch: 1.362,
-    shrink: 0.908,
-  },
-  {
-    type: 'box',
-    width: 91.88399999999999,
-  },
-  {
-    type: 'glue',
-    width: 2.724,
-    stretch: 1.362,
-    shrink: 0.908,
-  },
-  {
-    type: 'box',
-    width: 91.88399999999999,
-  },
-  {
-    type: 'glue',
-    width: 0,
-    stretch: 10000,
-    shrink: 0,
-  },
-  {
-    type: 'penalty',
-    width: 0,
-    penalty: -10000,
-  },
-];
-
-const widths = [78, 78];
+const width = 50;
 
 describe('bestFit', () => {
   test('should return at least one breakpoint', () => {
-    const nodeArr = [node1, node2, node3, node4, node5];
-    nodeArr.forEach(nodes => {
-      const breakpoints = applyBestFit(nodes, widths);
-      expect(breakpoints.length).toBeGreaterThan(0);
-    });
+    const node = [
+      {
+        type: 'box',
+        width: 25,
+      },
+      {
+        type: 'glue',
+        width: 0,
+        stretch: 10000,
+        shrink: 0,
+      },
+      {
+        type: 'penalty',
+        width: 0,
+        penalty: -10000,
+      },
+    ];
+
+    const breakpoints = applyBestFit(node, [width]);
+    expect(breakpoints.length).toBe(1);
   });
 
-  test('should break lines when the sum of widths is bigger than the given widths', () => {
-    const nodeArr = [node1, node2, node3, node4, node5];
-    nodeArr.forEach(nodes => {
-      const breakpoints = applyBestFit(nodes, widths);
-      let count = 0;
+  test('should break lines when the subnode is bigger than the given widths', () => {
+    const node = [
+      {
+        type: 'box',
+        width: 55,
+      },
+      {
+        type: 'glue',
+        width: 5,
+        stretch: 1,
+        shrink: 1,
+      },
+      {
+        type: 'box',
+        width: 55,
+      },
+      {
+        type: 'glue',
+        width: 5,
+        stretch: 1,
+        shrink: 1,
+      },
+      {
+        type: 'box',
+        width: 25,
+      },
+      {
+        type: 'glue',
+        width: 0,
+        stretch: 10000,
+        shrink: 0,
+      },
+      {
+        type: 'penalty',
+        width: 0,
+        penalty: -10000,
+      },
+    ];
 
-      for (let i = 0; i < nodes.length; i += 1) {
-        if (nodes[i].width > widths[0]) {
-          count += 1;
-        }
-      }
+    const breakpoints = applyBestFit(node, [width]);
 
-      expect(breakpoints.length).toBeGreaterThanOrEqual(count);
-    });
+    expect(breakpoints.length).toBe(3);
   });
 });

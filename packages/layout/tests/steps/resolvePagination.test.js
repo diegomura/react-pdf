6f0e8d2a130d39350cc4f61ff5c743b4b262c98a
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2022-01-15 13:42:17 +0300

    fix: use styles for forcing nodes height after split (#1659)
    * fix: use styles for forcing nodes height after split
    * remove console.log
    * add names for test
    * add changeset

--- packages/layout/tests/steps/resolvePagination.test.js
+++ packages/layout/tests/steps/resolvePagination.test.js
@@ -5,47 +5,47 @@ import resolveDimensions from '../../src/steps/resolveDimensions';
 const calcLayout = node => resolvePagination(resolveDimensions(node));
 
 describe('pagination step', () => {
-  const root = {
-    type: 'DOCUMENT',
-    children: [
-      {
-        type: 'PAGE',
-        box: {},
-        style: {
-          width: 100,
-          height: 100,
-        },
-        children: [
-          {
-            type: 'VIEW',
-            box: {},
-            style: {
-              position: 'absolute',
-              width: '50%',
-              top: 0,
-              bottom: 0,
-            },
-            props: {},
-            children: [],
-          },
-          {
-            type: 'TEXT',
-            box: {},
-            style: {},
-            props: {},
-            children: [
-              {
-                type: 'TEXT_INSTANCE',
-                value: 'hello world',
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  };
-
   test('should stretch absolute block to full page size', () => {
+    const root = {
+      type: 'DOCUMENT',
+      children: [
+        {
+          type: 'PAGE',
+          box: {},
+          style: {
+            width: 100,
+            height: 100,
+          },
+          children: [
+            {
+              type: 'VIEW',
+              box: {},
+              style: {
+                position: 'absolute',
+                width: '50%',
+                top: 0,
+                bottom: 0,
+              },
+              props: {},
+              children: [],
+            },
+            {
+              type: 'TEXT',
+              box: {},
+              style: {},
+              props: {},
+              children: [
+                {
+                  type: 'TEXT_INSTANCE',
+                  value: 'hello world',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+
     const layout = calcLayout(root);
 
     const page = layout.children[0];
@@ -53,5 +53,88 @@ describe('pagination step', () => {
 
     expect(page.box.height).toBe(100);
     expect(view.box.height).toBe(100);
+  });
+
+  test('should force new height for split nodes', () => {
+    const root = {
+      type: 'DOCUMENT',
+      children: [
+        {
+          type: 'PAGE',
+          box: {},
+          style: {
+            width: 15,
+            height: 60,
+          },
+
+          children: [
+            {
+              type: 'VIEW',
+              box: {},
+              style: {},
+              props: {},
+              children: [
+                {
+                  type: 'TEXT',
+                  box: {},
+                  style: {},
+                  props: {},
+                  children: [
+                    {
+                      type: 'TEXT_INSTANCE',
+                      value: 'a a a a',
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+
+    const layout = calcLayout(root);
+
+    const view1 = layout.children[0].children[0];
+    const view2 = layout.children[1].children[0];
+
+    expect(view1.box.height).toBe(60);
+    expect(view2.box.height).not.toBe(60);
+  });
+
+  test('should force new height for split nodes with fixed height', () => {
+    const root = {
+      type: 'DOCUMENT',
+      children: [
+        {
+          type: 'PAGE',
+          box: {},
+          style: {
+            width: 5,
+            height: 60,
+          },
+
+          children: [
+            {
+              type: 'VIEW',
+              box: {},
+              style: { height: 130 },
+              props: {},
+              children: [],
+            },
+          ],
+        },
+      ],
+    };
+
+    const layout = calcLayout(root);
+
+    const view1 = layout.children[0].children[0];
+    const view2 = layout.children[1].children[0];
+    const view3 = layout.children[2].children[0];
+
+    expect(view1.box.height).toBe(60);
+    expect(view2.box.height).toBe(60);
+    expect(view3.box.height).toBe(10);
   });
 });

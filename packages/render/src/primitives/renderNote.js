24f5c77706e12dbab45053cb704a2fe7cf60eb53
Diego Muracciole <diegomuracciole@gmail.com>
2022-06-05 19:00:53 -0300

    feat: add background color and border with Note support (#1865)

--- packages/render/src/primitives/renderNote.js
+++ packages/render/src/primitives/renderNote.js
@@ -1,8 +1,10 @@
 const renderNote = (ctx, node) => {
   const { top, left } = node.box;
   const value = node?.children?.[0].value || '';
+  const color = node.style?.backgroundColor || null;
+  const borderWidth = node.style?.borderWidth || null;
 
-  ctx.note(left, top, 0, 0, value);
+  ctx.note(left, top, 0, 0, value, { color, borderWidth });
 };
 
 export default renderNote;

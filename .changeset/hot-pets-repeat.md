e94e50a931df7347a8febc717ca76843502826c8
Diego Muracciole <diegomuracciole@gmail.com>
2022-07-02 23:39:20 -0300

    fix: media queries computing (#1910)

---
+++ .changeset/hot-pets-repeat.md
@@ -0,0 +1,5 @@
+---
+'@react-pdf/layout': patch
+---
+
+fix: media queries computing
--- packages/layout/src/steps/resolveStyles.js
+++ packages/layout/src/steps/resolveStyles.js
@@ -52,8 +52,10 @@ const resolveNodeStyles = container => node => {
  */
 const resolvePageStyles = page => {
   const dpi = page.props?.dpi || 72;
-  const box = page.box || page.style;
-  const container = { ...box, dpi };
+  const width = page.box?.width || page.style.width;
+  const height = page.box?.height || page.style.height;
+  const orientation = page.props?.orientation || 'portrait';
+  const container = { width, height, orientation, dpi };
 
   return resolveNodeStyles(container)(page);
 };

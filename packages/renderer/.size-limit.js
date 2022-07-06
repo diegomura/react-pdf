7174c9a685dc1ca028de9d6439a5850e3e875311
Carlo Beltrame <carlo@beltra.me>
2022-07-06 11:25:46 +0200

    Remove webpack 5 shims now that they are no longer needed

--- packages/renderer/.size-limit.js
+++ packages/renderer/.size-limit.js
@@ -11,23 +11,7 @@ module.exports = [
           // TODO: ignore iconv-lite on default because it contains exotic encodings like `win1251`, that isn't used in pdf
           'iconv-lite': false,
         },
-        fallback: {
-          process: require.resolve('process/browser'),
-          zlib: require.resolve('browserify-zlib'),
-          stream: require.resolve('stream-browserify'),
-          util: require.resolve('util'),
-          buffer: require.resolve('buffer'),
-          asset: require.resolve('assert'),
-        },
       };
-
-      config.plugins = [
-        new webpack.ProvidePlugin({
-          Buffer: ['buffer', 'Buffer'],
-          process: 'process/browser',
-        }),
-      ];
-
       return config;
     },
   },

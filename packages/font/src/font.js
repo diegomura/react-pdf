diegomura/react-pdf

commit 27403f9a6ac1bbcfb144afc201c4a3e5aca25cbd

feat: remove Buffer dependency from font package (#1927)

packages/font/src/font.js
@@ -24,12 +24,9 @@ const FONT_WEIGHTS = {
 
 const fetchFont = async (src, options) => {
   const response = await fetch(src, options);
+  const data = await response.arrayBuffer();
 
-  const buffer = await (response.buffer
-    ? response.buffer()
-    : response.arrayBuffer());
-
-  return buffer.constructor.name === 'Buffer' ? buffer : Buffer.from(buffer);
+  return new Uint8Array(data);
 };
 
 const isDataUrl = dataUrl => {
@@ -64,7 +61,7 @@ class FontSource {
     if (isDataUrl(this.src)) {
       const raw = this.src.split(',')[1];
       this.data = fontkit.create(toUint8Array(raw), postscriptName);
-    } else if (BROWSER || isUrl(this.src)) {
+    } else if (isUrl(this.src)) {
       const { headers, body, method = 'GET' } = this.options;
       const data = await fetchFont(this.src, { method, body, headers });
       this.data = fontkit.create(data, postscriptName);

commit faffc49c78c6da211da774b6a23ca40001b7fa8d

Restore the ability to load fonts by relative URL in browsers
This was broken in #1927, when trying to optimize for bundle size.

packages/font/src/font.js
@@ -61,7 +61,7 @@ class FontSource {
     if (isDataUrl(this.src)) {
       const raw = this.src.split(',')[1];
       this.data = fontkit.create(toUint8Array(raw), postscriptName);
-    } else if (isUrl(this.src)) {
+    } else if (BROWSER || isUrl(this.src)) {
       const { headers, body, method = 'GET' } = this.options;
       const data = await fetchFont(this.src, { method, body, headers });
       this.data = fontkit.create(data, postscriptName);
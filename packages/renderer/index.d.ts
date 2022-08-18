d7c4b8045da9a89c84a4a2cca8704a07cdc6392a
RingoDev <thomas.grininger@ringodev.com>
2022-08-18 12:24:38 +0200

    Add children prop to G and Svg Components

--- packages/renderer/index.d.ts
+++ packages/renderer/index.d.ts
@@ -239,6 +239,7 @@ declare namespace ReactPDF {
     height?: string | number;
     viewBox?: string;
     preserveAspectRatio?: string;
+    children?: React.ReactNode;
   }
 
   /**
@@ -342,7 +343,9 @@ declare namespace ReactPDF {
    */
   class Tspan extends React.Component<TspanProps> {}
 
-  interface GProps extends SVGPresentationAttributes {}
+  interface GProps extends SVGPresentationAttributes {
+    children?: React.ReactNode;
+  }
 
   /**
    * The <G /> SVG element is a container used to group other SVG elements.

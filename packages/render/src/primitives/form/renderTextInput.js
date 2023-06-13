c36d8c938e6bef612e85c64e007cece1235714a0
runelk <r.l.knudsen909@gmail.com>
2023-06-13 20:02:44 +0200

    Fix spelling error.

--- packages/render/src/primitives/form/renderTextInput.js
+++ packages/render/src/primitives/form/renderTextInput.js
@@ -1,15 +1,24 @@
 import { parseTextFieldOptions } from '../../utils/parseFormOptions';
 
 const renderTextInput = (ctx, node, options) => {
-    const { top, left, width, height } = node.box || {};
+  const { top, left, width, height } = node.box || {};
 
-    // Element's name
-    const name = node.props?.name || '';
+  // Element's name
+  const name = node.props?.name || '';
 
-    if (!options.formField)
-        throw new Error('The TextInput element must be a children of a FormField element.')
+  if (!options.formField)
+    throw new Error(
+      'The TextInput element must be a children of a FormField element.',
+    );
 
-    ctx.TextInput(name, left, top, width, height, parseTextFieldOptions(node, options.formField));
+  ctx.textInput(
+    name,
+    left,
+    top,
+    width,
+    height,
+    parseTextFieldOptions(node, options.formField),
+  );
 };
 
 export default renderTextInput;

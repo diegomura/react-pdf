8988fcd7ab73f295d7fff323b053d6adcf87d619
Mike Andrzejewski <mike@status101.com>
2023-08-24 13:49:42 +0200

    Add `table` support to `display` style

--- packages/types/style.d.ts
+++ packages/types/style.d.ts
@@ -20,7 +20,7 @@ export interface Style {
 
   aspectRatio?: number | string;
   bottom?: number | string;
-  display?: 'flex' | 'none';
+  display?: 'flex' | 'table' | 'none';
   left?: number | string;
   position?: 'absolute' | 'relative';
   right?: number | string;

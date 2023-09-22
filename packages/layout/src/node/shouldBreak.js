ff692343cde9341506d8fd99957c2c2385bce81e
Carlo Beltrame <carlo@beltra.me>
2023-09-22 13:46:51 +0200

    Refactor for better readability

--- packages/layout/src/node/shouldBreak.js
+++ packages/layout/src/node/shouldBreak.js
@@ -6,32 +6,42 @@ const getBreak = node => node.props?.break || false;
 
 const getMinPresenceAhead = node => node.props?.minPresenceAhead || 0;
 
+const getFurthestEnd = elements =>
+  Math.max(...elements.map(node => node.box.top + node.box.height));
+
+const getEndOfMinPresenceAhead = child => {
+  return (
+    child.box.top +
+    child.box.height +
+    child.box.marginBottom +
+    getMinPresenceAhead(child)
+  );
+};
+
+const getEndOfPresence = (child, futureElements) => {
+  const afterMinPresenceAhead = getEndOfMinPresenceAhead(child);
+  const endOfFurthestFutureElement = getFurthestEnd(
+    futureElements.filter(node => !node.props?.fixed),
+  );
+  return Math.min(afterMinPresenceAhead, endOfFurthestFutureElement);
+};
+
 const shouldBreak = (child, futureElements, height) => {
   if (child.props?.fixed) return false;
 
   const shouldSplit = height < child.box.top + child.box.height;
   const canWrap = getWrap(child);
-  const minPresenceAhead = getMinPresenceAhead(child);
-  const afterMinPresenceAhead =
-    child.box.top +
-    child.box.height +
-    child.box.marginBottom +
-    minPresenceAhead;
-  const nonFixedFutureElements = futureElements.filter(
-    node => !node.props?.fixed,
-  );
-  const endOfLastFutureElement = Math.max(
-    ...nonFixedFutureElements.map(node => node.box.top + node.box.height),
-  );
-  const afterPresence = Math.min(afterMinPresenceAhead, endOfLastFutureElement);
-  // If the child is already at the top of the page, breaking won't improve presence
+
+  // Calculate the y coordinate where the desired presence of the child ends
+  const endOfPresence = getEndOfPresence(child, futureElements);
+  // If the child is already at the top of the page, breaking won't improve its presence
   // (as long as react-pdf does not support breaking into differently sized containers)
   const breakingImprovesPresence = child.box.top > child.box.marginTop;
 
   return (
     getBreak(child) ||
     (shouldSplit && !canWrap) ||
-    (!shouldSplit && afterPresence > height && breakingImprovesPresence)
+    (!shouldSplit && endOfPresence > height && breakingImprovesPresence)
   );
 };
 

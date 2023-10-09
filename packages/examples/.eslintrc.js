973620768cb697f16660e19379a89669a8727d92
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-10-09 12:18:01 +0200

    Convert ESLint config to JSON

--- packages/examples/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['../../.eslintrc.js', 'plugin:react/recommended'],
-  rules: {
-    'react/prop-types': 0,
-    'jsx-a11y/anchor-is-valid': 0,
-    'react/jsx-one-expression-per-line': 0,
-  },
-};

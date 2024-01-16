6bfe7e8a30d96c04a1552800159992705f3605b1
Diego Muracciole <diegomuracciole@gmail.com>
2024-01-16 02:23:47 +0100

    fix: color parse error (#2498)
    * fix: color parse error
    
    * chore: bump color-string
    
    * chore: add changeset

---
+++ .changeset/fifty-bugs-change.md
@@ -0,0 +1,6 @@
+---
+'@react-pdf/stylesheet': patch
+'@react-pdf/render': patch
+---
+
+fix: color parse error
--- packages/render/src/utils/parseColor.js
+++ packages/render/src/utils/parseColor.js
@@ -1,7 +1,12 @@
 import colorString from 'color-string';
 
+const black = { value: [0, 0, 0], opacity: 1 };
+
 const parseColor = hex => {
   const parsed = colorString.get(hex);
+
+  if (!parsed) return black;
+
   const value = colorString.to.hex(parsed.value.slice(0, 3));
   const opacity = parsed.value[3];
 

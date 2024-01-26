f7be32445c3b5c2608111e9997cfee2ebfa86bd2
Wojciech Maj <kontakt@wojtekmaj.pl>
2024-01-26 13:28:32 +0100

    chore: test against multiple versions of React
    Closes #2568
    Requires #2567

--- packages/renderer/vitest.browser.config.js
+++ packages/renderer/vitest.browser.config.js
@@ -4,11 +4,28 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
+    alias:
+      process.env.REACT_VERSION && process.env.REACT_VERSION !== '18'
+        ? [
+            {
+              find: 'react/jsx-dev-runtime',
+              replacement: `react-${process.env.REACT_VERSION}/jsx-dev-runtime`,
+            },
+            {
+              find: 'react',
+              replacement: `react-${process.env.REACT_VERSION}`,
+            },
+            {
+              find: 'react-dom',
+              replacement: `react-dom-${process.env.REACT_VERSION}`,
+            },
+          ]
+        : undefined,
     conditions: ['browser'],
     mainFields: ['browser'],
   },
-  plugins: [react()],
   test: {
     environment: './tests/environment/jsdom.js',
     setupFiles: ['vitest.setup.js'],

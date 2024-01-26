7c35f379c01a31e22d4489f524bc17c5a8b95001
Wojciech Maj <kontakt@wojtekmaj.pl>
2024-01-26 13:28:32 +0100

    Test against multiple versions of React
    Closes #2568

--- packages/renderer/vitest.config.js
+++ packages/renderer/vitest.config.js
@@ -5,6 +5,25 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias:
+      process.env.REACT_VERSION && process.env.REACT_VERSION !== '18'
+        ? [
+            {
+              find: 'react/jsx-dev-runtime',
+              replacement: `react-${process.env.REACT_VERSION}/jsx-dev-runtime`,
+            },
+            {
+              find: 'react',
+              replacement: `react-${process.env.REACT_VERSION}`,
+            },
+            {
+              find: 'react-dom',
+              replacement: `react-dom-${process.env.REACT_VERSION}`,
+            },
+          ]
+        : undefined,
+  },
   test: {
     // Necessary to avoid "Module did not self-register" error with canvas.node
     pool: 'forks',

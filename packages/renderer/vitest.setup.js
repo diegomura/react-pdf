50a1e985c166a98959a63451ff43289da02eb46f
Wojciech Maj <kontakt@wojtekmaj.pl>
2024-01-26 13:28:32 +0100

    chore: test against multiple versions of React
    Closes #2568
    Requires #2567

--- packages/renderer/vitest.setup.js
+++ packages/renderer/vitest.setup.js
@@ -2,6 +2,10 @@ import { expect } from 'vitest';
 import path from 'path';
 import url from 'url';
 import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log(`Using React ${React.version} + ReactDOM ${ReactDOM.version}`);
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 

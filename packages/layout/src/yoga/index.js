862e2cca28afc59e0799c1ab3343efe76d907f57
Wojciech Maj <kontakt@wojtekmaj.pl>
2024-04-10 09:57:54 +0200

    feat!: update Yoga to 3.0
    Closes #2692
    Closes #2507
    
    This PR updates Yoga to 3.0.
    
    See the list of new features, improvements, and potential breaking changes in #2692.

--- packages/layout/src/yoga/index.js
+++ packages/layout/src/yoga/index.js
@@ -1,14 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 
-import * as Yoga from 'yoga-layout';
+import Yoga from 'yoga-layout';
 
 export const loadYoga = async () => {
-  const instance = await Yoga.loadYoga();
-  const config = instance.Config.create();
+  const config = Yoga.Config.create();
 
   config.setPointScaleFactor(0);
 
-  const node = { create: () => instance.Node.createWithConfig(config) };
+  const node = { create: () => Yoga.Node.createWithConfig(config) };
 
   return { node };
 };

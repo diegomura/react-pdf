ea227d1512f008eee0a9f2dbbfeb884a929c4d11
Wojciech Maj <kontakt@wojtekmaj.pl>
2024-06-19 12:51:10 +0200

    chore: add more JSDoc types

--- packages/fns/src/mapValues.js
+++ packages/fns/src/mapValues.js
@@ -1,3 +1,10 @@
+/**
+ * Maps over the values of an object and applies a function to each value.
+ *
+ * @param {Object} object the object to map over
+ * @param {Function} fn the function to apply to each value
+ * @returns {Object} a new object with the mapped values
+ */
 const mapValues = (object, fn) => {
   const entries = Object.entries(object);
 

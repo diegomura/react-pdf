9070cc2381cafa59025b3c1002a4fa2ce75e7117
Diego Muracciole <diegomuracciole@gmail.com>
2024-06-24 08:10:34 +0200

    feat: improve babel config (#2806)

--- babel.config.js
+++ babel.config.js
@@ -5,8 +5,8 @@ export default {
       {
         loose: true,
         targets: {
-          node: '14',
-          browsers: 'last 2 versions',
+          node: '18',
+          browsers: '>0.25%, not dead',
         },
       },
     ],
@@ -14,7 +14,6 @@ export default {
   ],
   plugins: [
     ['@babel/plugin-transform-runtime', { version: '^7.19.6' }],
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     '@babel/plugin-proposal-optional-chaining',
   ],

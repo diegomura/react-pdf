70f29a0407b1d56e9a7932b25c0d69132e9b4119
Diego Muracciole <diegomuracciole@gmail.com>
2024-09-22 19:07:23 +0200

    feat!: drop cjs support (#2871)

--- packages/font/rollup.config.js
+++ packages/font/rollup.config.js
@@ -2,19 +2,6 @@ import babel from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
 import pkg from './package.json' assert { type: 'json' };
 
-const cjs = {
-  exports: 'named',
-  format: 'cjs',
-  interop: 'compat',
-};
-
-const esm = {
-  format: 'es',
-};
-
-const getCJS = (override) => Object.assign({}, cjs, override);
-const getESM = (override) => Object.assign({}, esm, override);
-
 const babelConfig = () => ({
   babelrc: true,
   exclude: 'node_modules/**',
@@ -35,17 +22,14 @@ const getPlugins = ({ browser }) => [
 
 const serverConfig = {
   input: './src/index.js',
-  output: [getESM({ file: 'lib/index.js' }), getCJS({ file: 'lib/index.cjs' })],
+  output: { format: 'es', file: 'lib/index.js' },
   external,
   plugins: getPlugins({ browser: false }),
 };
 
 const browserConfig = {
   input: './src/index.js',
-  output: [
-    getESM({ file: 'lib/index.browser.js' }),
-    getCJS({ file: 'lib/index.browser.cjs' }),
-  ],
+  output: { format: 'es', file: 'lib/index.browser.js' },
   external,
   plugins: getPlugins({ browser: true }),
 };

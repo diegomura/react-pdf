7819bf08997812f18882758c5dc52683b0c02e2f
Diego Muracciole <diegomuracciole@gmail.com>
2024-11-16 21:51:41 +0100

    feat: add React 19 support (#2941)
    Co-authored-by: Alexander Nanberg <alex.nanberg@hotmail.com>

---
+++ .changeset/curly-pugs-hope.md
@@ -0,0 +1,6 @@
+---
+"@react-pdf/reconciler": minor
+"@react-pdf/renderer": minor
+---
+
+feat: add React 19 support
--- packages/reconciler/rollup.config.js
+++ packages/reconciler/rollup.config.js
@@ -6,13 +6,30 @@ import terser from '@rollup/plugin-terser';
 
 import trimReconciler from './build/trim-reconciler.js';
 
-export default {
-  input: 'src/index.js',
-  output: { format: 'es', file: 'lib/index.js' },
-  plugins: [
-    resolve({ resolveOnly: ['react-reconciler'] }),
-    commonjs({ esmExternals: (id) => id === 'scheduler' }),
-    trimReconciler(),
-    terser({ compress: { dead_code: true } }),
-  ],
-};
+export default [
+  {
+    input: 'src/index.js',
+    output: { format: 'es', file: 'lib/index.js' },
+    external: ['./reconciler-26', './reconciler-31'],
+  },
+  {
+    input: 'src/reconciler-26.js',
+    output: { format: 'es', file: 'lib/reconciler-26.js' },
+    plugins: [
+      resolve({ resolveOnly: ['react-reconciler-26'] }),
+      commonjs({ esmExternals: (id) => id === 'scheduler' }),
+      trimReconciler(),
+      terser({ compress: { dead_code: true } }),
+    ],
+  },
+  {
+    input: 'src/reconciler-31.js',
+    output: { format: 'es', file: 'lib/reconciler-31.js' },
+    plugins: [
+      resolve({ resolveOnly: ['react-reconciler-31'] }),
+      commonjs({ esmExternals: (id) => id === 'scheduler' }),
+      trimReconciler(),
+      terser({ compress: { dead_code: true } }),
+    ],
+  },
+];

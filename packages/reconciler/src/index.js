59dcb4af8e18875dc5159688f9179471c767308d
Diego Muracciole <diegomuracciole@gmail.com>
2024-11-16 22:30:22 +0100

    fix: reconciler build

--- packages/reconciler/src/index.js
+++ packages/reconciler/src/index.js
@@ -1,8 +1,9 @@
+/* eslint-disable import/extensions */
 /* eslint-disable import/no-extraneous-dependencies */
 
 import React from 'react';
-import createRendererForReact19 from './reconciler-31';
-import createRendererForReact18AndLess from './reconciler-26';
+import createRendererForReact19 from './reconciler-31.js';
+import createRendererForReact18AndLess from './reconciler-26.js';
 
 const isReact19 = React.version.startsWith('19');
 

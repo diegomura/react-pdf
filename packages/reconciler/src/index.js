1916e8405ce2b566110f2a1d66cfd8b4c4fa34d4
Diego Muracciole <diegomuracciole@gmail.com>
2024-11-26 01:34:00 +0100

    fix: conditional rendering (#2983)

--- packages/reconciler/src/index.js
+++ packages/reconciler/src/index.js
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import createRendererForReact19 from './reconciler-31.js';
-import createRendererForReact18AndLess from './reconciler-26.js';
+import createRendererForReact18AndLess from './reconciler-23.js';
 
 const isReact19 = React.version.startsWith('19');
 

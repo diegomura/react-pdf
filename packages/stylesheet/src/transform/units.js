18834efac2787a636c378718fee40cbe74f01ab8
Tim Nikischin <49103409+nikischin@users.noreply.github.com>
2024-12-03 01:19:11 +0100

    feat: allow units for page size (#2773)

--- packages/stylesheet/src/transform/units.js
+++ packages/stylesheet/src/transform/units.js
@@ -22,15 +22,16 @@ const parseValue = (value) => {
 const transformUnit = (container, value) => {
   const scalar = parseValue(value);
 
-  const dpi = 72; // Removed: container.dpi || 72
-  const mmFactor = (1 / 25.4) * dpi;
-  const cmFactor = (1 / 2.54) * dpi;
+  const outputDpi = 72;
+  const inputDpi = container.dpi || 72;
+  const mmFactor = (1 / 25.4) * outputDpi;
+  const cmFactor = (1 / 2.54) * outputDpi;
 
   switch (scalar.unit) {
     case 'rem':
       return scalar.value * (container.remBase || 18);
     case 'in':
-      return scalar.value * dpi;
+      return scalar.value * outputDpi;
     case 'mm':
       return scalar.value * mmFactor;
     case 'cm':
@@ -39,6 +40,8 @@ const transformUnit = (container, value) => {
       return scalar.value * (container.height / 100);
     case 'vw':
       return scalar.value * (container.width / 100);
+    case 'px':
+      return Math.round(scalar.value * (outputDpi / inputDpi));
     default:
       return scalar.value;
   }

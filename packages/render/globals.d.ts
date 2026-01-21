54124167fbf9f2b703800fe9835c97425036e272
TATSUNO “Taz” Yasuhiro <ytatsuno.jp@gmail.com>
2026-01-21 14:23:14 +0900

    feat(render): support hsla and hwba color models

--- packages/render/globals.d.ts
+++ packages/render/globals.d.ts
@@ -1,16 +1,5 @@
 declare module 'abs-svg-path';
 
-declare module 'color-string' {
-  function get(hex: string): { model: string; value: number[] } | null;
-
-  function toHex(color: number[]): string;
-
-  export default {
-    get,
-    to: { hex: toHex },
-  };
-}
-
 declare module 'normalize-svg-path' {
   export default function normalizePath(path: any[]): any[];
 }

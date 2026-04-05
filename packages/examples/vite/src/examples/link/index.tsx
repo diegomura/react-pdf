738d3f0f3aa6ec982dd7d867d192e5f9f528c0a4
Diego Muracciole <diegomuracciole@gmail.com>
2026-04-05 02:48:12 +0200

    feat: add link hitslop

--- packages/examples/vite/src/examples/link/index.tsx
+++ packages/examples/vite/src/examples/link/index.tsx
@@ -3,7 +3,7 @@ import { Document, Page, Link, Text, View } from '@react-pdf/renderer';
 
 const LinkExample = () => (
   <Document>
-    <Page>
+    <Page style={{ alignItems: 'flex-start' }}>
       <Link src="https://google.com">Some text link</Link>
 
       <Link src="https://google.com">
@@ -19,6 +19,18 @@ const LinkExample = () => (
 
       <Link src="https://google.com">
         <View style={{ width: 40, height: 40, backgroundColor: 'red' }} />
+      </Link>
+
+      <Link src="https://google.com" hitSlop={20} style={{ margin: 40 }}>
+        <View style={{ width: 40, height: 40, backgroundColor: 'blue' }} />
+      </Link>
+
+      <Link
+        src="https://google.com"
+        hitSlop={{ top: 10, bottom: 10 }}
+        style={{ marginVertical: 40 }}
+      >
+        Some text link with hitSlop
       </Link>
     </Page>
   </Document>

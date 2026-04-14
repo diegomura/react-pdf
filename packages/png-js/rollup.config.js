2e2a634cf9e607e7ec2584c64bfe66ef73edc410
Diego Muracciole <diegomuracciole@gmail.com>
2026-04-15 01:06:26 +0200

    feat(png-js): use fflate (#3373)

--- packages/png-js/rollup.config.js
+++ packages/png-js/rollup.config.js
@@ -1,10 +1,7 @@
 import babel from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
 import ignore from 'rollup-plugin-ignore';
-import alias from '@rollup/plugin-alias';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
-import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 
 import pkg from './package.json' with { type: 'json' };
 
@@ -17,28 +14,10 @@ const babelConfig = () => ({
 });
 
 const getExternal = ({ browser }) =>
-  browser
-    ? [
-        ...Object.keys(pkg.dependencies).filter(
-          (dep) => dep !== 'browserify-zlib',
-        ),
-      ]
-    : ['fs', ...Object.keys(pkg.dependencies)];
+  browser ? [...Object.keys(pkg.dependencies), 'zlib'] : ['fs', 'zlib'];
 
 const getPlugins = ({ browser }) => [
-  ...(browser
-    ? [
-        ignore(['fs']),
-        alias({
-          entries: [{ find: 'zlib', replacement: 'browserify-zlib' }],
-        }),
-        commonjs(),
-        nodeResolve({ browser, preferBuiltins: !browser }),
-        nodePolyfills({
-          include: [/node_modules\/.+\.js/, /\/png-js\/src\/.*\.js/],
-        }),
-      ]
-    : []),
+  ...(browser ? [ignore(['fs']), nodePolyfills({ include: null })] : []),
   babel(babelConfig()),
   replace({
     preventAssignment: true,
@@ -48,11 +27,16 @@ const getPlugins = ({ browser }) => [
   }),
 ];
 
+const getTreeshake = ({ browser }) => ({
+  moduleSideEffects: (id) => (browser ? id !== 'zlib' : id !== 'fflate'),
+});
+
 const serverConfig = {
   input,
   output: { format: 'es', file: 'lib/png-js.js' },
   external: getExternal({ browser: false }),
   plugins: getPlugins({ browser: false }),
+  treeshake: getTreeshake({ browser: false }),
 };
 
 const browserConfig = {
@@ -60,6 +44,7 @@ const browserConfig = {
   output: { format: 'es', file: 'lib/png-js.browser.js' },
   external: getExternal({ browser: true }),
   plugins: getPlugins({ browser: true }),
+  treeshake: getTreeshake({ browser: true }),
 };
 
 export default [serverConfig, browserConfig];

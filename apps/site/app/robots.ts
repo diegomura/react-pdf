e37ae36f920c906d9f3ec776717b4195c79adcea
Diego Muracciole <diegomuracciole@gmail.com>
2026-08-28 00:19:04 +0200

    feat(site): add sitemap, robots and OG image, take @react-pdf/ui from npm (#3539)

---
+++ apps/site/app/robots.ts
@@ -0,0 +1,8 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: { userAgent: '*', allow: '/' },
+    sitemap: 'https://react-pdf.org/sitemap.xml',
+  };
+}
